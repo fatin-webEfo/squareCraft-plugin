859d57402775af3635cb7d519019c8daf1f819ed
fatin-webEfo <fatin@webefo.com>
2025-03-12 14:19:53 +0600

    add

--- injectNavbarIcon.js
+++ injectNavbarIcon.js
@@ -48,7 +48,7 @@ export function injectNavbarIcon() {
             squareCraftDiv.appendChild(icon);
             squareCraftDiv.appendChild(text);
 
-            toolbarContainer.appendChild(squareCraftDiv);
+            toolbarContainer.parentNode.insertBefore(squareCraftDiv);
         }
     }
 }
