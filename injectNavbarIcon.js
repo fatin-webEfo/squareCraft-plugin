d7e1e68d01ed3b7b8b32e1825d5a8c7418bcc575
fatin-webEfo <fatin@webefo.com>
2025-03-15 09:25:39 +0600

    border radsus

--- injectNavbarIcon.js
+++ injectNavbarIcon.js
@@ -32,6 +32,7 @@ export function injectNavbarIcon() {
                 squareCraftDiv.style.display = "flex";
                 squareCraftDiv.style.alignItems = "center";
                 squareCraftDiv.style.border = "1px solid #E5E4E2";
+                squareCraftDiv.style.borderRadius = "6px";
                 squareCraftDiv.style.padding = "6px";
                 squareCraftDiv.style.gap = "6px";
     
