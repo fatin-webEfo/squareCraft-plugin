ec07ef4085041e41351e2298bbb55ba0c6435511
fatin-webEfo <fatin@webefo.com>
2025-03-17 15:58:19 +0600

    add

--- squareCraft.js
+++ squareCraft.js
@@ -36,21 +36,6 @@
   }
   // Token and Ids
 
-//   document.body.addEventListener("click", (event) => {
-//     let block = event.target.closest('[id^="block-"]');
-//     if (!block) return;
-
-//     if (selectedElement) {
-//       selectedElement.style.outline = "";
-//       selectedElement.classList.remove("squareCraft-selected");
-//     }
-
-//     selectedElement = block;
-//     selectedElement.style.outline = "2px dashed #EF7C2F";
-//     selectedElement.classList.add("squareCraft-selected");
-
-//     console.log(`✅ Selected Element: ${selectedElement.id}`);
-//   });
 
   document.addEventListener("DOMContentLoaded", function () {
     const selectedElement = document.querySelector(
