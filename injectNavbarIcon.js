92d47fd10445b9dfb86e5154e4690c0b084ca05b
fatin-webEfo <fatin@webefo.com>
2025-03-19 15:53:28 +0600

    add

--- injectNavbarIcon.js
+++ injectNavbarIcon.js
@@ -41,7 +41,7 @@ export function injectNavbarIcon() {
                 message.style.boxShadow = "0px 4px 10px rgba(0, 0, 0, 0.3)";
                 message.style.top = "60px";
                 message.style.right = "10%";
-                message.style.transform = "translateX(-50%)";
+                message.style.transform = "translateX(-30%)";
 
                 navContainer.parentNode.insertBefore(message, navContainer);
 
