035e9efde17b7b1bfc4efd953c56ff936efef15d
fatin-webEfo <fatin@webefo.com>
2025-03-22 11:09:14 +0600

    addd

--- squareCraft.js
+++ squareCraft.js
@@ -59,18 +59,18 @@ console.log("parent" , Url)
   function getTextType(tagName, element) {
     let classList = element?.classList || [];
 
-    if (tagName === "h1") return { type: "heading1", borderColor: "#FF0000" };
-    if (tagName === "h2") return { type: "heading2", borderColor: "#FFA500" };
-    if (tagName === "h3") return { type: "heading3", borderColor: "#FFFF00" };
-    if (tagName === "h4") return { type: "heading4", borderColor: "#008000" };
+    if (tagName === "h1") return { type: "heading1" };
+    if (tagName === "h2") return { type: "heading2"};
+    if (tagName === "h3") return { type: "heading3"};
+    if (tagName === "h4") return { type: "heading4"};
 
     if (tagName === "p") {
       if (classList.contains("sqsrte-large")) {
-        return { type: "paragraph1", borderColor: "#4B0082" };
+        return { type: "paragraph1"};
       } else if (classList.contains("sqsrte-small")) {
-        return { type: "paragraph3", borderColor: "#0000FF" };
+        return { type: "paragraph3" };
       } else {
-        return { type: "paragraph2", borderColor: "#9400D3" };
+        return { type: "paragraph2"};
       }
     }
     return null;
