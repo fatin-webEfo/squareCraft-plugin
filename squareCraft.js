a3c41ce29425821a00d26f79467b2d78ba100a30
fatin-webEfo <fatin@webefo.com>
2025-04-06 16:22:28 +0600

    add

--- squareCraft.js
+++ squareCraft.js
@@ -58,25 +58,8 @@
 
   });
 
-  function getTextType(tagName, element) {
-    let classList = element?.classList || [];
+  const { getTextType } = await import ('https://your-cdn.com/squareCraft-plugin/src/utils/textType.js');
 
-    if (tagName === "h1") return { type: "heading1", borderColor: "#FF0000" };
-    if (tagName === "h2") return { type: "heading2", borderColor: "#FFA500" };
-    if (tagName === "h3") return { type: "heading3", borderColor: "#FFFF00" };
-    if (tagName === "h4") return { type: "heading4", borderColor: "#008000" };
-
-    if (tagName === "p") {
-      if (classList.contains("sqsrte-large")) {
-        return { type: "paragraph1", borderColor: "#4B0082" };
-      } else if (classList.contains("sqsrte-small")) {
-        return { type: "paragraph3", borderColor: "#0000FF" };
-      } else {
-        return { type: "paragraph2", borderColor: "#9400D3" };
-      }
-    }
-    return null;
-  }
 
 
   let lastClickedBlockId = null;
