395bef1cf5c046fc805c11a03e6bc428de9554ae
fatin-webEfo <fatin@webefo.com>
2025-04-09 12:42:04 +0600

    add

--- src/components/Typo/typoItalicSelect.js
+++ src/components/Typo/typoItalicSelect.js
@@ -70,7 +70,7 @@ export function typoItalicSelect(fontSizes) {
                Regular</p>
          </div>
          <div class="sc-bg-3f3f3f sc-px-2" style="height: 27px; padding: 0 8px;">
-            <img class="sc-mx-auto sc-rotate-180 sc-mt-2" width="10px"
+            <img class="sc-mx-auto sc-rotate-180 sc-mt-6" width="10px"
                src="https://fatin-webefo.github.io/squareCraft-plugin/public/arrow.svg" alt="">
          </div>
       </div>
