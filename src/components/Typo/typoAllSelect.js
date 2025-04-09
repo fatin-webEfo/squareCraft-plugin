0bdd54a45260270031409f11239e0fe8c17da74f
fatin-webEfo <fatin@webefo.com>
2025-04-09 12:27:06 +0600

    add

--- src/components/Typo/typoAllSelect.js
+++ src/components/Typo/typoAllSelect.js
@@ -61,10 +61,10 @@ export function typoAllSelect(fontSizes,LetterSpacing){
    </div>
      
       <div
-         class="sc-mt-2  sc-grid   sc-grid-cols-12 sc-gap-2 ">
+         class="sc-mt-2  sc-grid sc-grid-cols-12 sc-gap-2 ">
          <div
             class="sc-flex sc-bg-494949 sc-pl-2 sc-col-span-7  sc-justify-between sc-border sc-border-solid sc-border-585858 sc-rounded-6px sc-items-center ">
-            <div class="    ">
+            <div >
                <p
                   class="sc-text-sm sc-universal sc-poppins sc-font-light">
                   Regular</p>
