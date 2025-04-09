e8798538e58adffb59618a70eff41e429a037758
fatin-webEfo <fatin@webefo.com>
2025-04-09 15:06:44 +0600

    add

--- src/components/Typo/typoBoldSelect.js
+++ src/components/Typo/typoBoldSelect.js
@@ -112,7 +112,7 @@ export function typoBoldSelect(fontSizes) {
 
    <div class="sc-mt-4 ">
       <p class="sc-text-xs sc-font-thin sc-mt-4 sc-universal sc-text-gray-300 sc-poppins">Text Highlight</p>
-      <div class="sc-py-1 sc-mt-2 sc-bg-3f3f3f sc-inActiveTab-border sc-flex sc-justify-between sc-items-center sc-px-2 sc-rounded-6px">
+      <div class="sc-py-1 sc-mt-2 sc-bg-3f3f3f sc-inActiveTab-border sc-w-50 sc-flex sc-justify-between sc-items-center sc-px-2 sc-rounded-6px">
          <p class="sc-text-sm sc-poppins sc-universal">#363544</p>
          <div class="sc-square-6 sc-border-colors sc-cursor-pointer"></div>
       </div>
