9dcac68b5365e69255923e9566591386fc4c2f43
fatin-webEfo <fatin@webefo.com>
2025-04-12 12:16:29 +0600

    add

--- squareCraft.js
+++ squareCraft.js
@@ -369,8 +369,8 @@ function loadWidgetFromString(htmlString, clickedBlock) {
 }
 
 
-document.body.addEventListener("click", (event) => {
-  const block = event.target.closest('[id^="block-"]');
+document.body.addEventListener("click", (e) => {
+  const block = e.target.closest('[id^="block-"]');
   if (!block) return;
 
   const nestedElements = block.querySelectorAll("h1, h2, h3, h4, p, img, a, button");
@@ -379,26 +379,39 @@ document.body.addEventListener("click", (event) => {
     const tagName = el.tagName.toLowerCase();
     const classList = el.classList;
 
-    if (tagName === "h1") console.log({ type: "heading1", element: el });
-    else if (tagName === "h2") console.log({ type: "heading2", element: el });
-    else if (tagName === "h3") console.log({ type: "heading3", element: el });
-    else if (tagName === "h4") console.log({ type: "heading4", element: el });
-    else if (tagName === "p") {
-      if (classList.contains("sqsrte-large")) {
-        console.log({ type: "paragraph1", element: el });
-      } else if (classList.contains("sqsrte-small")) {
-        console.log({ type: "paragraph3", element: el });
-      } else {
-        console.log({ type: "paragraph2", element: el });
-      }
-    } else if (tagName === "img") {
-      console.log({ type: "image", element: el });
-    } else if (tagName === "a" || tagName === "button") {
-      console.log({ type: "button", element: el });
+    if (tagName === "h1") return console.log({ type: "text", tag: "h1", element: el });
+    if (tagName === "h2") return console.log({ type: "text", tag: "h2", element: el });
+    if (tagName === "h3") return console.log({ type: "text", tag: "h3", element: el });
+    if (tagName === "h4") return console.log({ type: "text", tag: "h4", element: el });
+
+    if (tagName === "p") {
+      if (
+        classList.contains("rte-placeholder") || 
+        classList.contains("ProseMirror-widget") ||
+        el.innerText.trim() === ""
+      ) return;
+
+      if (classList.contains("sqsrte-large"))
+        return console.log({ type: "text", tag: "p", sub: "paragraph1", element: el });
+      else if (classList.contains("sqsrte-small"))
+        return console.log({ type: "text", tag: "p", sub: "paragraph3", element: el });
+      else
+        return console.log({ type: "text", tag: "p", sub: "paragraph2", element: el });
+    }
+
+    if (tagName === "img") {
+      if (classList.contains("ProseMirror-separator")) return;
+      return console.log({ type: "image", element: el });
+    }
+
+    if (tagName === "a" || tagName === "button") {
+      if (el.querySelector("img")) return; // Skip anchor used just to wrap image
+      return console.log({ type: "button", element: el });
     }
   });
 });
-  
+
+
 
   function makeWidgetDraggable() {
     if (!widgetContainer) return;
