7d0c5e8bbce21943b9c9b204e024bb3c98b010d4
fatin-webEfo <fatin@webefo.com>
2025-04-12 14:23:27 +0600

    add

--- generateHeadingDropdown.js
+++ generateHeadingDropdown.js
@@ -2,6 +2,7 @@ import {typoAllSelect} from "https://fatin-webefo.github.io/squareCraft-plugin/s
 import {typoBoldSelect} from "https://fatin-webefo.github.io/squareCraft-plugin/src/components/Typo/typoBoldSelect.js"
 import {typoItalicSelect} from "https://fatin-webefo.github.io/squareCraft-plugin/src/components/Typo/typoItalicSelect.js"
 import {typoLinkSelect} from "https://fatin-webefo.github.io/squareCraft-plugin/src/components/Typo/typoLinkSelect.js"
+
 export function createHeadingDropdown(id, fontSizes = [], LetterSpacing = []) {
   return `
     <div id="${id}">
