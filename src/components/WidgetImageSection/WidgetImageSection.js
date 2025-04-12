77b9574b487ce71fa15824aadc5844382cbd76e7
fatin-webEfo <fatin@webefo.com>
2025-04-12 14:53:36 +0600

    add

--- src/components/WidgetImageSection/WidgetImageSection.js
+++ src/components/WidgetImageSection/WidgetImageSection.js
@@ -8,7 +8,7 @@ export function WidgetImageSection(id){
      <div id="imageSection">
          <div  class="sc-flex sc-p-2 sc-items-center sc-justify-between">
             <div class="sc-flex sc-gap-2 sc-items-center">
-               <img loading="lazy" src="https://fatin-webefo.github.io/squareCraft-plugin/public/imageicon.png" alt="">
+               <img loading="lazy" src="https://fatin-webefo.github.io/squareCraft-plugin/public/iamgeicon.png" alt="">
                <p class="sc-universal sc-poppins">Image</p>
             </div>
             <img src="https://fatin-webefo.github.io/squareCraft-plugin/public/arrow.svg" alt="">
