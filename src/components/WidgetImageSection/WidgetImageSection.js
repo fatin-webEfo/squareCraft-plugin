101e10355abb7a17aa0a3b96da800ed9cc8af471
fatin-webEfo <fatin@webefo.com>
2025-04-13 15:02:06 +0600

    add

--- src/components/WidgetImageSection/WidgetImageSection.js
+++ src/components/WidgetImageSection/WidgetImageSection.js
@@ -46,7 +46,7 @@ export function WidgetImageSection(id){
             </div>
 
             <div class="sc-flex sc-mt-4 sc-px-2">
-               <div
+               <div 
                   class="sc-bg-3f3f3f sc-flex sc-border-hover-EF7C2F sc-border sc-border-solid sc-border-3f3f3f sc-cursor-pointer sc-px-2 sc-justify-between sc-py-1 sc-w-full sc-rounded-6px">
                   <h5 class="sc-poppins sc-font-thin sc-universal  sc-text-color-white">Border</h5>
                   <img id="paragraph1Arrow" src="https://fatin-webefo.github.io/squareCraft-plugin/public/arrow.svg"
@@ -68,14 +68,16 @@ export function WidgetImageSection(id){
                   class="sc-flex sc-cursor-pointer sc-items-center sc-rounded-15px sc-gap-2 sc-px-2 sc-py-0_5 sc-bg sc-bg-454545">
                   <p id="radiousCount" class="sc-font-thin sc-poppins sc-universal sc-text-xs">50px</p>
                   <div class="sc-flex sc-flex-col sc-items-center sc-gap-1">
-                     <img src="https://fatin-webefo.github.io/squareCraft-plugin/public/arrow.svg" alt="" width="8">
-                     <img src="https://fatin-webefo.github.io/squareCraft-plugin/public/arrow.svg" width="8"
-                        class="sc-rotate-180" alt="">
+                     <img  src="https://fatin-webefo.github.io/squareCraft-plugin/public/arrow.svg"
+                        alt="" width="8">
+                     <img  src="https://fatin-webefo.github.io/squareCraft-plugin/public/arrow.svg"
+                        width="8" class="sc-rotate-180" alt="">
                   </div>
                </div>
             </div>
             <div class="sc-mt-4 sc-px-2">
                <div id="radiousField" class="sc-rounded-15px sc-relative sc-w-full sc-h-2 sc-bg-F6F6F6">
+               <div id="radiousFill" class="sc-absolute sc-bg-color-EF7C2F sc-h-2 sc-rounded-l-full"></div>
                   <div id="radiousBullet"
                      class="sc-absolute sc-bg-color-EF7C2F sc-w-3 sc-h-3 sc-rounded-full sc-cursor-pointer sc-top-half">
                   </div>
@@ -118,158 +120,64 @@ export function WidgetImageSection(id){
                </div>
                <div class="sc-mt-4 sc-gap-2 sc-grid sc-grid-cols-12">
                   <div class="sc-col-span-5">
-                     <p class="sc-poppins sc-font-thin sc-universal  sc-text-sm sc-font-thin sc-text-gray-300"> Border
-                        Color
+                     <p class="sc-poppins sc-font-thin sc-universal  sc-text-sm sc-font-thin sc-text-gray-300"> Border Color
                      </p>
                      <div
-                        class="sc-py-xs sc-mt-3 sc-bg-3f3f3f sc-inActiveTab-border  sc-flex sc-justify-between sc-items-center sc-px-1_5 sc-rounded-6px">
+                        class="sc-py-0_5 sc-mt-3 sc-bg-3f3f3f sc-inActiveTab-border  sc-flex sc-justify-between sc-items-center sc-px-1_5 sc-rounded-6px">
                         <p class="sc-text-sm sc-poppins c-font-light sc-universal">#363544</p>
                         <div class="sc-square-6 sc-border-colors sc-cursor-pointer"></div>
                      </div>
                   </div>
-                  <div class="sc-col-span-7">
-                     <p class="sc-poppins sc-font-thin sc-universal  sc-text-sm sc-font-thin sc-text-gray-300"> Border
-                        Style
-                     </p>
+                  <div class="sc-col-span-7"> <p class="sc-poppins sc-font-thin sc-universal  sc-text-sm sc-font-thin sc-text-gray-300"> Border Style
+                  </p>
                      <div style="padding: 3px 0px;"
-                        class="sc-bg-3f3f3f sc-flex sc-text-xs sc-gap-1 sc-mt-3 sc-rounded-6px   sc-border sc-border-solid sc-border-3f3f3f   sc-justify-between  sc-px-1 ">
+                     class="sc-bg-3f3f3f sc-flex sc-text-xs sc-gap-1 sc-mt-3 sc-rounded-6px   sc-border sc-border-solid sc-border-3f3f3f   sc-justify-between  sc-px-1 ">
+                   
+                 
+                     <div class="sc-py-0_5 sc-bg-454545 sc-w-full sc-rounded-6px "><p  class="sc-font-thin sc-poppins  sc-text-center sc-universal  sc-cursor-pointer   ">Solid</p></div>
+                
+                 
+                     <div class="sc-py-0_5  sc-w-full sc-rounded-6px "><p  class="sc-font-thin sc-poppins  sc-text-center sc-universal  sc-cursor-pointer  ">Dashed</p></div>
+               <div class="sc-py-0_5  sc-w-full sc-rounded-6px">
 
-
-                        <div class="sc-py-0_5 sc-bg-454545 sc-w-full sc-rounded-6px ">
-                           <p class="sc-font-thin sc-poppins  sc-text-center sc-universal  sc-cursor-pointer   ">Solid
-                           </p>
-                        </div>
-
-
-                        <div class="sc-py-0_5  sc-w-full sc-rounded-6px ">
-                           <p class="sc-font-thin sc-poppins  sc-text-center sc-universal  sc-cursor-pointer  ">Dashed
-                           </p>
-                        </div>
-                        <div class="sc-py-0_5  sc-w-full sc-rounded-6px">
-
-                           <p
-                              class="sc-font-thin sc-poppins  sc-text-center sc-universal  sc-cursor-pointer  sc-rounded-6px">
-                              Dotted</p>
-                        </div>
-
-                     </div>
+                     <p  class="sc-font-thin sc-poppins  sc-text-center sc-universal  sc-cursor-pointer  sc-rounded-6px">Dotted</p>
+               </div>
+                  
+                  </div>
                   </div>
                </div>
 
-               <div class="sc-mt-4">
-                  <div class="  sc-flex sc-items-center sc-justify-between">
-                     <div class="sc-flex sc-gap-2 sc-items-center">
-                        <p class="sc-poppins sc-font-thin sc-universal  sc-text-sm sc-font-thin sc-text-gray-300">
-                           Border Color
-                        </p>
-                        <div
-                           class="sc-flex sc-cursor-pointer sc-items-center sc-rounded-15px sc-gap-1 sc-p-1_5  sc-bg sc-bg-454545">
-
-                           <img src="https://fatin-webefo.github.io/squareCraft-plugin/public/reset.svg" width="10"
-                              alt="reset">
-                        </div>
-                     </div>
-                     <div
-                        class="sc-flex sc-cursor-pointer sc-items-center sc-rounded-15px sc-gap-2 sc-px-2 sc-py-0_5 sc-bg sc-bg-454545">
-                        <p class="sc-font-thin sc-poppins sc-universal sc-text-xs">50px</p>
-                        <div class="sc-flex sc-flex-col sc-items-center sc-gap-1">
-                           <img src="https://fatin-webefo.github.io/squareCraft-plugin/public/arrow.svg" alt=""
-                              width="8">
-                           <img src="https://fatin-webefo.github.io/squareCraft-plugin/public/arrow.svg" width="8"
-                              class="sc-rotate-180" alt="">
-                        </div>
-                     </div>
-                  </div>
-                  <div class="sc-rounded-15px sc-mt-3 sc-relative sc-w-full sc-h-2 sc-bg-F6F6F6">
-                     <div
-                        class="sc-absolute sc-bg-color-EF7C2F sc-w-3 sc-h-3 sc-rounded-full sc-cursor-pointer sc-top-half">
-                     </div>
-                  </div>
-
-               </div>
-
-               <div class="sc-mt-4 ">
+              <div class="sc-mt-4"> 
+               <div class="  sc-flex sc-items-center sc-justify-between">
+               <div class="sc-flex sc-gap-2 sc-items-center">
+                  <p class="sc-poppins sc-font-thin sc-universal  sc-text-sm sc-font-thin sc-text-gray-300"> Border Color
+                  </p>
                   <div
-                     class="sc-bg-3f3f3f sc-flex sc-border-hover-EF7C2F sc-border sc-border-solid sc-border-3f3f3f sc-cursor-pointer sc-px-2 sc-justify-between sc-py-1  sc-rounded-6px">
-                     <h5 class="sc-poppins sc-font-thin sc-universal  sc-text-color-white">Overlay</h5>
-                     <img id="paragraph1Arrow" src="https://fatin-webefo.github.io/squareCraft-plugin/public/arrow.svg"
-                        class="sc-rotate-180" alt="">
-                  </div>
-                  <div class="sc-flex sc-gap-2 sc-items-center sc-mt-3">
-                     <p class="sc-poppins sc-font-thin sc-universal  sc-text-sm sc-font-thin sc-text-gray-300"> Overlay
-                     </p>
-                     <div
-                        class="sc-flex sc-cursor-pointer sc-items-center sc-rounded-15px sc-gap-1 sc-p-1_5  sc-bg sc-bg-454545">
+                     class="sc-flex sc-cursor-pointer sc-items-center sc-rounded-15px sc-gap-1 sc-p-1_5  sc-bg sc-bg-454545">
 
-                        <img src="https://fatin-webefo.github.io/squareCraft-plugin/public/reset.svg" width="10"
-                           alt="reset">
-                     </div>
-                  </div>
-                  <div class="sc-mt-4">
-                     <p class="sc-poppins sc-font-thin sc-universal  sc-text-sm sc-font-thin sc-text-gray-300"> Visible
-                     </p>
-                  </div>
-                  <div class="sc-flex sc-mt-2 sc-items-center">
-                     <div class="sc-bg-3f3f3f sc-py-1px sc-px-2 sc-rounded-l">
-                        <p class="sc-universal sc-poppins sc-text-sm ">Yes</p>
-                     </div>
-                     <img style="width: 26px;" loading="lazy"
-                        src="https://fatin-webefo.github.io/squareCraft-plugin/public/imageArrow.svg" alt="">
+                     <img src="https://fatin-webefo.github.io/squareCraft-plugin/public/reset.svg" width="10"
+                        alt="reset">
                   </div>
                </div>
-               <div class="sc-mt-4 sc-gap-2 sc-flex">
-                  <div class="">
-                     <p class="sc-poppins sc-font-thin sc-universal  sc-text-sm sc-font-thin sc-text-gray-300">
-                        Color
-                     </p>
-                     <div
-                        class="sc-py-xs sc-gap-5 sc-mt-2 sc-bg-3f3f3f sc-inActiveTab-border  sc-flex sc-justify-between sc-items-center sc-px-1_5 sc-rounded-6px">
-                        <p class="sc-text-xs sc-poppins sc-font-light sc-universal">#363544</p>
-                        <div class="sc-square-6 sc-border-colors sc-cursor-pointer"></div>
-                     </div>
+               <div
+                  class="sc-flex sc-cursor-pointer sc-items-center sc-rounded-15px sc-gap-2 sc-px-2 sc-py-0_5 sc-bg sc-bg-454545">
+                  <p  class="sc-font-thin sc-poppins sc-universal sc-text-xs">50px</p>
+                  <div class="sc-flex sc-flex-col sc-items-center sc-gap-1">
+                     <img  src="https://fatin-webefo.github.io/squareCraft-plugin/public/arrow.svg"
+                        alt="" width="8">
+                     <img  src="https://fatin-webefo.github.io/squareCraft-plugin/public/arrow.svg"
+                        width="8" class="sc-rotate-180" alt="">
                   </div>
-                  <div class="">
-                     <p class="sc-poppins sc-font-thin sc-universal  sc-text-sm sc-font-thin sc-text-gray-300">
-                        Width
-                     </p>
-                     <div class="sc-flex sc-mt-2 sc-items-center">
-                        <div class="sc-bg-3f3f3f sc-py-1 sc-rounded-l sc-px-2">
-                           <p class="sc-universal sc-poppins sc-text-sm ">20px</p>
-                        </div>
-                        <div class="sc-bg-454545 sc-px-2_5 sc-py-3">
-                           <div class="sc-flex sc-flex-col sc-items-center sc-gap-2">
-                              <img src="https://fatin-webefo.github.io/squareCraft-plugin/public/arrow.svg" alt=""
-                                 width="10">
-                              <img src="https://fatin-webefo.github.io/squareCraft-plugin/public/arrow.svg" width="10"
-                                 class="sc-rotate-180" alt="">
-                           </div>
-                        </div>
-                     </div>
-                    
-                  </div>
-                  <div class="">
-                     <p class="sc-poppins sc-font-thin sc-universal  sc-text-sm sc-font-thin sc-text-gray-300">
-                        Height
-                     </p>
-                     <div class="sc-flex sc-mt-2 sc-items-center">
-                        <div class="sc-bg-3f3f3f sc-py-1 sc-rounded-l sc-px-2">
-                           <p class="sc-universal sc-poppins sc-text-sm ">20px</p>
-                        </div>
-                        <div class="sc-bg-454545 sc-px-2_5 sc-py-3">
-                           <div class="sc-flex sc-flex-col sc-items-center sc-gap-2">
-                              <img src="https://fatin-webefo.github.io/squareCraft-plugin/public/arrow.svg" alt=""
-                                 width="10">
-                              <img src="https://fatin-webefo.github.io/squareCraft-plugin/public/arrow.svg" width="10"
-                                 class="sc-rotate-180" alt="">
-                           </div>
-                        </div>
-                     </div>
-                     <div>
-
-                     </div>
-                  </div>
+               </div>
+            </div><div  class="sc-rounded-15px sc-mt-3 sc-relative sc-w-full sc-h-2 sc-bg-F6F6F6">
+               <div 
+                  class="sc-absolute sc-bg-color-EF7C2F sc-w-3 sc-h-3 sc-rounded-full sc-cursor-pointer sc-top-half">
                </div>
             </div>
+         
+         </div>
+            </div>
+
             <div class="sc-mt-4"> </div>
          </div>
     `
