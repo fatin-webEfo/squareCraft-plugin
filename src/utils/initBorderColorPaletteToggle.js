9d9c25be3782b27427715e420e559e404249dc33
fatin-webEfo <fatin@webefo.com>
2025-04-16 11:49:51 +0600

    add

--- src/utils/initBorderColorPaletteToggle.js
+++ src/utils/initBorderColorPaletteToggle.js
@@ -11,8 +11,8 @@ export function initBorderColorPaletteToggle(themeColors) {
         const swatch = document.createElement("div");
         swatch.className = "sc-border-colors sc-hidden sc-cursor-pointer"; 
         swatch.style.backgroundColor = color;
-        swatch.style.width = "20px";
-        swatch.style.height = "20px";
+        swatch.style.width = "15px";
+        swatch.style.height = "15px";
         swatch.style.borderRadius = "6px";
         swatch.style.border = "1px solid white"; 
         swatch.title = color; 
