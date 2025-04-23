a3eb79310b19348479ca72846291399a963025bd
fatin-webEfo <fatin@webefo.com>
2025-04-23 14:23:22 +0600

    add

--- html.js
+++ html.js
@@ -3,7 +3,7 @@ import { getToggleState, setToggleState } from 'https://fatin-webefo.github.io/s
 import { WidgetTypoSection } from 'https://fatin-webefo.github.io/squareCraft-plugin/src/components/WidgetTypoSection/WidgetTypoSection.js';
 import { WidgetImageSection } from 'https://fatin-webefo.github.io/squareCraft-plugin/src/components/WidgetImageSection/WidgetImageSection.js';
 import { WidgetButtonSection } from 'https://fatin-webefo.github.io/squareCraft-plugin/src/components/WidgetButtonSection/WidgetButtonSection.js';
-
+let currentButtonType = "Button";
 
 export function html() {
 
@@ -34,7 +34,7 @@ export function html() {
   
       ${WidgetTypoSection("typoSection")}
       ${WidgetImageSection("imageSection")}
-      ${WidgetButtonSection("buttonSection")}
+      ${WidgetButtonSection("buttonSection", currentButtonType)}
   
    </div>
    <div class="sc-mt-4">
