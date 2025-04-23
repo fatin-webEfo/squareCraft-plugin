600518afff9035798f533f2fa8202bd8ede28460
fatin-webEfo <fatin@webefo.com>
2025-04-23 18:50:32 +0600

    add

--- src/components/WidgetButtonSection/WidgetButtonSection.js
+++ src/components/WidgetButtonSection/WidgetButtonSection.js
@@ -1,17 +1,17 @@
 
-// import { getCurrentButtonType  } from "https://fatin-webefo.github.io/squareCraft-plugin/src/components/BlockType/detectBlockElementTypes.js";
+import { getCurrentButtonType  } from "https://fatin-webefo.github.io/squareCraft-plugin/src/components/BlockType/detectBlockElementTypes.js";
 
 
 export  function WidgetButtonSection() {
-//    let buttonType = await getCurrentButtonType();
+   let buttonType =  getCurrentButtonType();
    
  
-//    console.log("DEBUG buttonType:", buttonType, typeof buttonType);
-//    const displayType = ["Primary Button", "Secondary Button", "Tertiary Button"].includes(buttonType)
-//    ? buttonType
-//    : "Button";
+   console.log("DEBUG buttonType:", buttonType, typeof buttonType);
+   const displayType = ["Primary Button", "Secondary Button", "Tertiary Button"].includes(buttonType)
+   ? buttonType
+   : "Button";
 
-//  console.log("✅ Final Button Type to Display:", displayType);
+ console.log("✅ Final Button Type to Display:", displayType);
 
 
    return `
@@ -20,7 +20,7 @@ export  function WidgetButtonSection() {
          <div  class="sc-flex sc-p-2 sc-items-center sc-justify-between">
             <div class="sc-flex sc-gap-2 sc-items-center">
                <img loading="lazy" src="https://fatin-webefo.github.io/squareCraft-plugin/public/buttonicon.png" alt="">
-               <p class="sc-universal sc-roboto">Button</p>
+               <p class="sc-universal sc-roboto">${displayType}</p>
             </div>
             <img src="https://fatin-webefo.github.io/squareCraft-plugin/public/arrow.svg" alt="">
          </div>
