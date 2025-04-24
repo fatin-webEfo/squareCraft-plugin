2f187049b84ef45de131b35e09487445d1d11dea
fatin-webEfo <fatin@webefo.com>
2025-04-24 14:31:37 +0600

    add

--- src/components/WidgetButtonSection/WidgetButtonSection.js
+++ src/components/WidgetButtonSection/WidgetButtonSection.js
@@ -2,7 +2,7 @@ export function WidgetButtonSection() {
    const LetterSpacing = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"];
    const fontSizes = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13"];
 
-   
+
    return `
       <div id="buttonSection">
             <div class="sc-flex sc-p-2 sc-items-center sc-justify-between">
