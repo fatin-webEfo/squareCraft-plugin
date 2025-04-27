0295929b4090f3fc4336597e5772d8db8ba28b6d
fatin-webEfo <fatin@webefo.com>
2025-04-27 11:06:58 +0600

    add

--- src/utils/initButtonSectionToggleControls/initImageUploadPreview.js
+++ src/utils/initButtonSectionToggleControls/initImageUploadPreview.js
@@ -9,7 +9,7 @@ export function initImageUploadPreview() {
     const hiddenInput = document.createElement("input");
     hiddenInput.type = "file";
     hiddenInput.accept = "image/*";
-    hiddenInput.style.display = "none"; 
+    hiddenInput.style.display = "none";
   
     document.body.appendChild(hiddenInput);
   
@@ -23,18 +23,6 @@ export function initImageUploadPreview() {
       if (file) {
         const reader = new FileReader();
         reader.onload = function(e) {
-          let previewImg = document.getElementById("imagePreviewSmall");
-          if (!previewImg) {
-            previewImg = document.createElement("img");
-            previewImg.id = "imagePreviewSmall";
-            previewImg.loading = "lazy";
-            previewImg.style.width = "30px"; 
-            previewImg.style.objectFit = "cover";
-            previewImg.style.marginTop = "10px";
-  
-            uploadButton.parentNode.appendChild(previewImg);
-          }
-          previewImg.src = e.target.result;
         };
         reader.readAsDataURL(file);
       }
