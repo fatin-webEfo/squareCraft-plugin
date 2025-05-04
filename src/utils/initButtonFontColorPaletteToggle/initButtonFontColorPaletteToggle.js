b448063e5127895707f65558e7e0ecae3aefe35d
fatin-webEfo <fatin@webefo.com>
2025-05-04 16:00:52 +0600

    add

--- src/utils/initButtonFontColorPaletteToggle/initButtonFontColorPaletteToggle.js
+++ src/utils/initButtonFontColorPaletteToggle/initButtonFontColorPaletteToggle.js
@@ -280,28 +280,29 @@ export function initButtonFontColorPaletteToggle(themeColors, selectedElement) {
 
 
   function getGradientCanvas(hue, width, height) {
-    const canvas = selectedElement.querySelector("canvas");
-const ctx = canvas?.getContext("2d");
-
+    const canvas = document.createElement("canvas");
+    const ctx = canvas.getContext("2d");
+  
     canvas.width = width;
     canvas.height = height;
-
+  
     const gradient1 = ctx.createLinearGradient(0, 0, width, 0);
     gradient1.addColorStop(0, `hsl(${hue}, 100%, 50%)`);
     gradient1.addColorStop(1, "white");
-
+  
     const gradient2 = ctx.createLinearGradient(0, height, 0, 0);
     gradient2.addColorStop(0, "black");
     gradient2.addColorStop(1, "transparent");
-
+  
     ctx.fillStyle = gradient1;
     ctx.fillRect(0, 0, width, height);
     ctx.globalCompositeOperation = "multiply";
     ctx.fillStyle = gradient2;
     ctx.fillRect(0, 0, width, height);
-
+  
     return canvas;
   }
+  
 
 
   function moveBullet(offsetX, offsetY) {
