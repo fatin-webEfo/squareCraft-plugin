4d5fafa40ab6c30aaf7da558710069302d04359e
fatin-webEfo <fatin@webefo.com>
2025-05-06 11:33:35 +0600

    add

--- src/utils/initButtonStyles/initButtonStyles.js
+++ src/utils/initButtonStyles/initButtonStyles.js
@@ -241,14 +241,13 @@ export function initButtonIconDimensionToggle(getSelectedElement) {
 
   const sizeValues = Array.from({ length: 51 }, (_, i) => i + 10); // 10 to 60
 
-  // Common dropdown style
   const dropdownClassList = ["sc-absolute", "sc-left-0", "sc-hidden", "sc-h-44", "sc-bg-colo-EF7C2F-hover", "sc-top-12", "sc-z-99999", "sc-scrollBar"];
 
   function generateDropdownHTML(values, type) {
     return values.map(
       (val) => `
-      <div data-${type}="${val}" class="sc-bg-3f3f3f sc-py-1 sc-border sc-border-solid sc-border-EF7C2F sc-px-2 sc-w-20 sc-cursor-pointer hover:sc-bg-555">
-        <p class="sc-universal sc-roboto sc-bg-colo-EF7C2F-hover sc-text-sm">${val}px</p>
+      <div data-${type}="${val}" class="sc-bg-3f3f3f sc-py-1 sc-bg-colo-EF7C2F-hover sc-border sc-border-solid sc-border-EF7C2F sc-px-2 sc-w-20 sc-cursor-pointer hover:sc-bg-555">
+        <p class="sc-universal sc-roboto  sc-text-sm">${val}px</p>
       </div>`
     ).join("");
   }
