47da386d24d134d8fa040c50067a4250185b663c
fatin-webEfo <fatin@webefo.com>
2025-05-17 15:25:51 +0600

    add

--- src/components/WidgetButtonSection/WidgetButtonSection.js
+++ src/components/WidgetButtonSection/WidgetButtonSection.js
@@ -14,7 +14,7 @@ export function WidgetButtonSection() {
             <img src="https://fatin-webefo.github.io/squareCraft-plugin/public/arrow.svg" alt="">
          </div>
          <div class="sc-h-1px sc-bg-3f3f3f"></div>
-         <div class="sc-flex sc-px-2 sc-items-center sc-justify-between">
+         <div class="sc-flex sc-px-2 sc-items-center sc-justify-between sc-h-12">
             <div class="sc-flex sc-gap-2 sc-items-center">
                <div class="toggle-container" id="toggleSwitch">
                   <div class="toggle-bullet"></div> 
