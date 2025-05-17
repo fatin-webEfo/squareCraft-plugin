0455d2288b6fa8f87397508e3b618d3f8c2bda6c
fatin-webEfo <fatin@webefo.com>
2025-05-17 16:20:04 +0600

    add

--- src/components/WidgetButtonNormalState/WidgetButtonNormalState.js
+++ src/components/WidgetButtonNormalState/WidgetButtonNormalState.js
@@ -419,7 +419,7 @@ export function WidgetButtonNormalState(){
                <p class="sc-font-size-11 sc-font-thin sc-mt-4 sc-universal sc-text-gray-300 sc-roboto">Icon Color
                </p>
                <div
-                  class="sc-py-0_5 sc-mt-2 sc-w-30 sc-bg-3f3f3f sc-inActiveTab-border  sc-flex sc-justify-between sc-items-center sc-px-2 sc-rounded-6px">
+                  class="sc-py-4px sc-mt-2 sc-w-30 sc-bg-3f3f3f sc-inActiveTab-border  sc-flex sc-justify-between sc-items-center sc-px-2 sc-rounded-6px">
                   <p class="sc-font-size-12 sc-roboto sc-font-light sc-universal">#363544</p>
                   <div class="sc-square-6 sc-border-colors sc-cursor-pointer"></div>
                </div>
