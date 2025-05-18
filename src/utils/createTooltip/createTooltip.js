9a8562b617e7b906f9f872188ae01007bec84ed0
fatin-webEfo <fatin@webefo.com>
2025-05-18 15:37:57 +0600

    add

--- src/utils/createTooltip/createTooltip.js
+++ src/utils/createTooltip/createTooltip.js
@@ -1,4 +1,18 @@
-export function createTooltip(text) {
-    return `<div class="sc-tooltip">${text}</div>`;
+export function createTooltip(targetEl, text) {
+    if (!targetEl) return;
+  
+    const tooltip = document.createElement("div");
+    tooltip.className = "sc-tooltip";
+    tooltip.innerText = text;
+  
+    targetEl.appendChild(tooltip);
+  
+    targetEl.addEventListener("mouseenter", () => {
+      tooltip.style.opacity = "1";
+    });
+  
+    targetEl.addEventListener("mouseleave", () => {
+      tooltip.style.opacity = "0";
+    });
   }
   
