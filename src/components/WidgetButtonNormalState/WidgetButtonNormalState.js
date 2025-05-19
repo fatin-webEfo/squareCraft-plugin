688dbaab0dde52ef4f26818b11028478b5f6a171
fatin-webEfo <fatin@webefo.com>
2025-05-19 11:17:02 +0600

    add

--- src/components/WidgetButtonNormalState/WidgetButtonNormalState.js
+++ src/components/WidgetButtonNormalState/WidgetButtonNormalState.js
@@ -274,8 +274,11 @@
 
                         <div class="sc-relative">
                            <div id="iconLibraryButton"
-                           class="sc-h-9 sc-flex sc-items-center sc-justify-center  sc-roudned-r-md sc-px-1_5 sc-inActiveTab-border sc-cursor-pointer">
-
+                           class="sc-h-9 sc-flex sc-tooltip-target sc-items-center sc-justify-center  sc-roudned-r-md sc-px-1_5 sc-inActiveTab-border sc-cursor-pointer">
+                               <div class="sc-tooltip sc-hidden">
+                       <div class="tooltip-arrow"></div>
+                        icon library
+                     </div>
                            <img src="https://fatin-webefo.github.io/squareCraft-plugin/public/iconLibrary.svg"
                               class=" alignment-icon   sc-mx-auto" alt="right">
 
