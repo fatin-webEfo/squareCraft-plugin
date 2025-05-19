26d1f856d3a7c2a83dd374f099e5cdc270c278b5
fatin-webEfo <fatin@webefo.com>
2025-05-19 16:09:59 +0600

    add

--- src/components/WidgetButtonNormalState/WidgetButtonNormalState.js
+++ src/components/WidgetButtonNormalState/WidgetButtonNormalState.js
@@ -311,17 +311,17 @@
                          ${solidIconList.map((i) => `
                              <img 
                              src="https://fatin-webefo.github.io/squareCraft-plugin/public/solidIcons/solidicon%20(${i}).svg" 
-                             class="sc-rounded-md sc-mx-auto sc-bg-3f3f3f sc-px-1 sc-py-4px" 
+                             class="sc-rounded-md sc-cursor-pointer  sc-mx-auto sc-bg-3f3f3f sc-px-1 sc-py-4px" 
                              width="22" 
                              height="22" 
                              alt="">`).join("")}
                          </div>
 
-                         <div id="buttonIconOutlineoptions" class="sc-hidden sc-scrollBar sc-justify-between sc-rounded-md sc-gap-2 sc-h-44  sc-grid-cols-6 sc-bg-color-2c2c2c">
+                         <div id="buttonIconOutlineoptions" class="sc-hidden sc-scrollBar sc-justify-between sc-rounded-md  sc-gap-2 sc-h-44  sc-grid-cols-6 sc-bg-color-2c2c2c">
                           ${outlineIcons.map((i) => `
                              <img 
                              src="https://fatin-webefo.github.io/squareCraft-plugin/public/outlineIcons/outlineicon%20(${i}).svg" 
-                             class="sc-rounded-md sc-mx-auto sc-bg-3f3f3f sc-px-1 sc-py-4px" 
+                             class="sc-rounded-md sc-cursor-pointer sc-mx-auto sc-bg-3f3f3f sc-px-1 sc-py-4px" 
                              width="22" 
                              height="22" 
                              alt="">`).join("")}
