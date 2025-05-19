b653db3e0d861392d928a8abb889215c79bb6d5b
fatin-webEfo <fatin@webefo.com>
2025-05-19 12:35:28 +0600

    add

--- src/components/WidgetButtonNormalState/WidgetButtonNormalState.js
+++ src/components/WidgetButtonNormalState/WidgetButtonNormalState.js
@@ -7,7 +7,7 @@
 
 
    return `
-   <div id="ButtonNormalState" class="sc-mt-4 sc-px-2">
+  <div id="ButtonNormalState" class="sc-mt-4 sc-px-2">
             <div class="sc-flex ">
 
                <div id="fontButton"
@@ -90,33 +90,42 @@
             </div>
             
 
-            <div class="sc-flex sc-text-color-white sc-px-1 
-            sc-rounded-6px sc-relative sc-border sc-border-solid sc-border-585858 
-            sc-items-center ">
-               <div class="sc-Letter-spacing-container sc-flex sc-justify-between sc-items-center sc-flex sc-items-center sc-border 
-               sc-border-solid sc-border-3d3d3d  sc-rounded-6px 
-               ">
-                  <input type="text" id="scButtonLetterSpacingInput" value="15px" class="sc-Letter-spacing-input sc-font-light sc-font-size-12 sc-text-color-white 
-                  sc-bg-transparent sc-w-full  sc-py-1px sc-font-light">
-                  <div id="scButtonLetterSpacingSelect" class="sc-mx-auto sc-tooltip-target sc-ml-1 sc-flex sc-items-center sc-justify-center">
-                    <div class="sc-tooltip sc-hidden">
+            <div class="sc-flex sc-text-color-white sc-rounded-6px sc-relative sc-border sc-border-solid sc-border-585858 sc-items-center">
+               <div class="sc-flex sc-text-color-white sc-justify-between sc-col-span-4 sc-rounded-6px sc-items-center">
+                 <div class="sc-font-size-container sc-roboto sc-universal sc-flex sc-justify-between sc-items-center sc-rounded-6px">
+                   
+                   <input type="text" id="scButtonLetterSpacingInput" value="15"
+                     class="sc-font-size-input sc-font-light sc-z-99999 sc-font-size-12 sc-text-color-white sc-bg-transparent  sc-universal sc-font-light">
+             
+                   <div class="sc-v-line"></div>
+             
+                   <div class="sc-flex sc-items-center sc-justify-center sc-ml-2">
+                     <p class="sc-font-light sc-text-center sc-font-size-12 sc-universal">px</p>
+                   </div>
+             
+                   <div id="scButtonLetterSpacingSelect"
+                     class="sc-bg-3f3f3f sc-flex sc-items-center sc-justify-center sc-cursor-pointer sc-px-1_5 sc-tooltip-target"
+                     style="height: 28px; margin-left: 12px; border-radius: 0px 5px 5px 0px;">
+                     <div class="sc-tooltip sc-hidden">
                        <div class="tooltip-arrow"></div>
                        Letter spacing
                      </div>
                      <img id="scButtonLetterSpacingDropdown" loading="lazy"
-                        src="https://fatin-webefo.github.io/squareCraft-plugin/public/line-spacing.svg"
-                        style="width: 12px;"
-                        class=" sc-px-1 sc-flex sc-items-center sc-justify-center sc-mt-1 sc-mx-auto sc-cursor-pointer">
-                  </div>
+                       src="https://fatin-webefo.github.io/squareCraft-plugin/public/line-spacing.svg"
+                       style="width: 12px;" class=" sc-mx-auto sc-cursor-pointer">
+                   </div>
+                 </div>
+             
+                 <div id="scButtonLetterSpacingOptions"
+                   class="sc-absolute sc-hidden sc-scrollBar sc-z-99999 sc-border sc-border-solid sc-border-EF7C2F sc-h-44 sc-font-sm sc-bg-3f3f3f sc-w-12 sc-rounded-6px sc-border-585858 sc-mt-1">
+                   ${ButtonLetterSpacing?.map((gap) => `
+                     <div class="sc-dropdown-item sc-py-1px sc-text-center sc-font-size-12" data-value="${gap}">
+                       ${gap}
+                     </div>`).join("")}
+                 </div>
                </div>
-               <div id="scButtonLetterSpacingOptions" class="sc-hidden sc-scrollBar sc-z-99999 sc-border sc-border-solid sc-border-EF7C2F sc-h-44 sc-font-sm sc-bg-3f3f3f sc-w-12
-               sc-rounded-6px sc-border sc-border-585858 sc-absolute 
-               sc-mt-1">
-                  ${ButtonLetterSpacing?.map((gap) => `
-                  <div class="sc-dropdown-item sc-py-1px sc-text-center  sc-font-size-12" data-value="${gap}">${gap}
-                  </div>`).join("")}
-               </div>
-            </div>
+             </div>
+             
 
          </div>
          <div class=" sc-flex sc-mt-2 sc-gap-2 ">
