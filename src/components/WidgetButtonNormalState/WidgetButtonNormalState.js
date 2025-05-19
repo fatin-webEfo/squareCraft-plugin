c436cfced620fbbd466baf6e7f18426aa429e846
fatin-webEfo <fatin@webefo.com>
2025-05-19 18:11:46 +0600

    add

--- src/components/WidgetButtonNormalState/WidgetButtonNormalState.js
+++ src/components/WidgetButtonNormalState/WidgetButtonNormalState.js
@@ -800,10 +800,11 @@ export function WidgetButtonNormalState() {
                         <div
                            class="sc-flex sc-cursor-pointer sc-items-center sc-rounded-15px sc-gap-2 sc-px-2 sc-py-0_5 sc-bg sc-bg-454545">
                            <p id="buttonShadowSpreadCount" class="sc-font-thin sc-roboto sc-universal sc-font-size-11">0px</p>
-                           <div class="sc-flex sc-flex-col sc-items-center sc-gap-1">
-                             <span class="sc-arrow-placeholder"></span>
-                              <span class="sc-arrow-placeholder sc-rotate-180"></span>
-                           </div>
+                          <div class="sc-flex sc-flex-col sc-items-center sc-gap-1">
+  <span class="sc-arrow-placeholder"></span>
+  <span class="sc-arrow-placeholder sc-rotate-180"></span>
+</div>
+
                         </div>
                      </div>
                      <div id="buttonShadowSpreadField" class="sc-rounded-15px sc-mt-2 sc-relative sc-w-full sc-h-2 sc-bg-F6F6F6">
