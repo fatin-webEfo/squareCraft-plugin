a608263e6f510f7bd8c0a66ca84b9e94d446becb
fatin-webEfo <fatin@webefo.com>
2025-05-22 12:41:50 +0600

    add

--- src/utils/initButtonStyles/initButtonHoverStyles.js
+++ src/utils/initButtonStyles/initButtonHoverStyles.js
@@ -420,8 +420,6 @@ export function initHoverButtonIconSpacingControl(getSelectedElement) {
 }
 
 
-
-
 export function initHoverButtonBorderRadiusControl(getSelectedElement) {
   if (hoverRadiusInitialized) return;
   hoverRadiusInitialized = true;
@@ -469,6 +467,7 @@ export function initHoverButtonBorderRadiusControl(getSelectedElement) {
 
     const selected = getSelectedElement?.();
     if (!selected) return;
+
     const btn = selected.querySelector("a.sqs-button-element--primary, a.sqs-button-element--secondary, a.sqs-button-element--tertiary");
     if (!btn) return;
 
@@ -500,12 +499,21 @@ export function initHoverButtonBorderRadiusControl(getSelectedElement) {
     update(Math.round((x / rect.width) * max));
   });
 
-  incBtn?.addEventListener("click", () => update(value + 1));
-  decBtn?.addEventListener("click", () => update(value - 1));
+  incBtn?.addEventListener("click", () => {
+    const newValue = Math.min(value + 1, max);
+    update(newValue);
+  });
+
+  decBtn?.addEventListener("click", () => {
+    const newValue = Math.max(value - 1, 0);
+    update(newValue);
+  });
+
   resetBtn?.addEventListener("click", () => update(0));
 
   update(value);
 }
+
 
 
 
