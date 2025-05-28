90fd1365e278091358652a6e3a60d2e789dba742
fatin-webEfo <fatin@webefo.com>
2025-05-28 11:38:56 +0600

    add

--- src/components/WidgetButtonNormalState/WidgetButtonNormalState.js
+++ src/components/WidgetButtonNormalState/WidgetButtonNormalState.js
@@ -193,7 +193,7 @@ export function WidgetButtonNormalState() {
        class="sc-absolute sc-border sc-border-solid sc-border-EF7C2F sc-left-0 sc-w-full sc-text-sm sc-z-999999 sc-rounded-15px sc-bg-494949 sc-flex sc-hidden sc-top-6 sc-flex-col sc-overflow-hidden">
     <div class="sc-cursor-pointer sc-dropdown-item sc-py-2px sc-px-2" data-format="RGB">RGB</div>
     <div class="sc-cursor-pointer sc-dropdown-item sc-py-2px sc-px-2" data-format="HSL">HSL</div>
-    <div class="sc-cursor-pointer sc-dropdown-item sc-py-2px sc-px-2" data-format="HEX">HEX</div>
+    <div class="sc-cursor-pointer sc-dropdown-item sc-py-2px sc-px-2 sc-rounded-b-14px" data-format="HEX">HEX</div>
   </div>
 </div>
 
