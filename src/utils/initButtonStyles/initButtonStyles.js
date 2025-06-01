641801728b5f0c2578109f213d1cbbcb875d9094
fatin-webEfo <fatin@webefo.com>
2025-06-01 12:35:33 +0600

    add

--- src/utils/initButtonStyles/initButtonStyles.js
+++ src/utils/initButtonStyles/initButtonStyles.js
@@ -1505,6 +1505,7 @@ export function resetAllButtonStyles(getSelectedElement) {
       const iconLabel = document.getElementById("iconPositionLabel");
       if (iconLabel) {
         iconLabel.textContent = "Select";
+        iconLabel.fontSize = "12px"
       }
 
       ["buttonIconTransformNone", "buttoniconRotationTypeNone"].forEach(
