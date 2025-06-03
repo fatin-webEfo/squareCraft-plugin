94b1e1fd81e2318db2810abbbf5b1c3588807c37
fatin-webEfo <fatin@webefo.com>
2025-06-03 16:05:33 +0600

    add

--- src/components/WidgetButtonSection/WidgetButtonAdvanceSection/WidgetButtonAdvanceSection.js
+++ src/components/WidgetButtonSection/WidgetButtonAdvanceSection/WidgetButtonAdvanceSection.js
@@ -2,7 +2,7 @@ export function WidgetButtonAdvanceSection(){
 
 
     return `
-    <div id="button-advance-section">
+  <div id="button-advance-section">
         <div class="sc-flex sc-p-2 sc-items-center sc-justify-between">
           <div class="sc-flex sc-gap-2 sc-items-center">
             <img
@@ -63,10 +63,31 @@ export function WidgetButtonAdvanceSection(){
 
              
            </div>
-          <div id="button-advance-vertical" class="sc-bg-454545 sc-p-2 sc-rounded-4px"> <div class="sc-w-full">
+        
+          
+          <div id="button-advance-vertical" class="sc-bg-454545 sc-p-2 sc-rounded-4px">
+            
+            <div class=" sc-flex sc-flex-col sc-gap-2">
+              <p class="sc-roboto sc-font-thin sc-universal  sc-font-size-14 ">Custom Timeline</p>
+            
+              <div class="sc-relative sc-mt-2 sc-h-2 sc-bg-F6F6F6 sc-rounded-15px">
+                <div id="timeline-start-fill" class="sc-absolute sc-bg-color-EF7C2F sc-h-2 sc-rounded-l-full" style="left: 0%; width: 10%;"></div>
+                <div id="timeline-end-fill" class="sc-absolute sc-bg-F6B67B sc-h-2 sc-rounded-r-full" style="right: 0%; width: 30%;"></div>
+            
+                <div id="timeline-start-bullet" class="sc-absolute sc-w-3 sc-h-3 sc-bg-color-EF7C2F sc-rounded-full sc-cursor-pointer sc-top-half" style="left: 10%;"></div>
+                <div id="timeline-end-bullet" class="sc-absolute sc-w-3 sc-h-3 sc-bg-F6B67B sc-rounded-full sc-cursor-pointer sc-top-half" style="right: 30%;"></div>
+              </div>
+            
+              <div class="sc-flex sc-mt-1 sc-justify-between sc-font-size-12 sc-text-gray-300 ">
+                <p class="sc-universal sc-roboto">Start <span id="timelineStartValue">10%</span></p>
+                <p class="sc-universal sc-roboto">End <span id="timelineEndValue">30%</span></p>
+              </div>
+            </div>
+            
+            <div class="sc-w-full sc-mt-3">
             <div class=" sc-flex sc-w-full sc-items-center sc-justify-between">
                <div class="sc-flex sc-gap-2 sc-items-center">
-                  <p class="sc-roboto sc-font-thin sc-universal  sc-font-size-12 sc-font-thin sc-text-gray-300"> Entry
+                  <p class="sc-roboto sc-font-thin sc-universal  sc-font-size-14 "> Entry
                   </p>
                   <div id="icon-size-reset"
                      class="sc-flex sc-cursor-pointer sc-gradiant-border sc-items-center sc-rounded-15px sc-gap-1 sc-p-1_5 sc-bg-3f3f3f">
@@ -94,7 +115,7 @@ export function WidgetButtonAdvanceSection(){
            <div class="sc-w-full sc-mt-3">
             <div class=" sc-flex sc-w-full sc-items-center sc-justify-between">
                <div class="sc-flex sc-gap-2 sc-items-center">
-                  <p class="sc-roboto sc-font-thin sc-universal  sc-font-size-12 sc-font-thin sc-text-gray-300"> Center
+                  <p class="sc-roboto sc-font-thin sc-universal  sc-font-size-14 "> Center
                   </p>
                   <div id="icon-size-reset"
                      class="sc-flex sc-cursor-pointer sc-gradiant-border sc-items-center sc-rounded-15px sc-gap-1 sc-p-1_5 sc-bg-3f3f3f">
@@ -122,7 +143,7 @@ export function WidgetButtonAdvanceSection(){
            <div class="sc-w-full sc-mt-3">
             <div class=" sc-flex sc-w-full sc-items-center sc-justify-between">
                <div class="sc-flex sc-gap-2 sc-items-center">
-                  <p class="sc-roboto sc-font-thin sc-universal  sc-font-size-12 sc-font-thin sc-text-gray-300"> Exit
+                  <p class="sc-roboto sc-font-thin sc-universal  sc-font-size-14 "> Exit
                   </p>
                   <div id="icon-size-reset"
                      class="sc-flex sc-cursor-pointer sc-gradiant-border sc-items-center sc-rounded-15px sc-gap-1 sc-p-1_5 sc-bg-3f3f3f">
@@ -150,7 +171,7 @@ export function WidgetButtonAdvanceSection(){
            <div class="sc-w-full sc-mt-3">
             <div class=" sc-flex sc-w-full sc-items-center sc-justify-between">
                <div class="sc-flex sc-gap-2 sc-items-center">
-                  <p class="sc-roboto sc-font-thin sc-universal  sc-font-size-12 sc-font-thin sc-text-gray-300"> Effect Speed
+                  <p class="sc-roboto sc-font-thin sc-universal  sc-font-size-14 "> Effect Speed
                   </p>
                   <div id="icon-size-reset"
                      class="sc-flex sc-cursor-pointer sc-gradiant-border sc-items-center sc-rounded-15px sc-gap-1 sc-p-1_5 sc-bg-3f3f3f">
