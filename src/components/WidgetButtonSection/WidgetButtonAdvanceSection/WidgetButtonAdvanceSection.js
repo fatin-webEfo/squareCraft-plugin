ab2658195b81e33e72db2bc280683ba1e23b94bb
fatin-webEfo <fatin@webefo.com>
2025-06-03 15:53:30 +0600

    add

--- src/components/WidgetButtonSection/WidgetButtonAdvanceSection/WidgetButtonAdvanceSection.js
+++ src/components/WidgetButtonSection/WidgetButtonAdvanceSection/WidgetButtonAdvanceSection.js
@@ -2,7 +2,7 @@ export function WidgetButtonAdvanceSection(){
 
 
     return `
-   <div id="button-advance-section">
+    <div id="button-advance-section">
         <div class="sc-flex sc-p-2 sc-items-center sc-justify-between">
           <div class="sc-flex sc-gap-2 sc-items-center">
             <img
@@ -42,7 +42,7 @@ export function WidgetButtonAdvanceSection(){
         </div>
         <div class="sc-px-2 sc-mt-2 sc-flex-col sc-gap-2">
           <div class="sc-flex ">
-             <div
+             <div id="button-advance-vertica"
                class="sc-bg-3f3f3f sc-relative sc-z-99999 sc-flex sc-border-hover-3d3d3d sc-border sc-border-solid sc-border-3f3f3f sc-cursor-pointer sc-px-2 sc-justify-between sc-py-1 sc-w-full sc-rounded-4px">
                <div class="sc-flex sc-items-center sc-gap-8px">
                  <img
@@ -63,20 +63,20 @@ export function WidgetButtonAdvanceSection(){
 
              
            </div>
-          <div class="sc-bg-454545 sc-p-2 sc-rounded-4px"> <div class="sc-w-full">
+          <div id="button-advance-vertical" class="sc-bg-454545 sc-p-2 sc-rounded-4px"> <div class="sc-w-full">
             <div class=" sc-flex sc-w-full sc-items-center sc-justify-between">
                <div class="sc-flex sc-gap-2 sc-items-center">
                   <p class="sc-roboto sc-font-thin sc-universal  sc-font-size-12 sc-font-thin sc-text-gray-300"> Entry
                   </p>
                   <div id="icon-size-reset"
-                     class="sc-flex sc-cursor-pointer sc-gradiant-border sc-items-center sc-rounded-15px sc-gap-1 sc-p-1_5  sc-bg sc-bg-454545">
+                     class="sc-flex sc-cursor-pointer sc-gradiant-border sc-items-center sc-rounded-15px sc-gap-1 sc-p-1_5 sc-bg-3f3f3f">
 
                      <img src="https://fatin-webefo.github.io/squareCraft-plugin/public/reset.svg" width="10"
                         alt="reset">
                   </div>
                </div>
                <div
-                  class="sc-flex sc-cursor-pointer sc-items-center sc-rounded-15px sc-gap-2 sc-px-2 sc-py-0_5 sc-bg sc-bg-454545">
+                  class="sc-flex sc-cursor-pointer sc-items-center sc-rounded-15px sc-gap-2 sc-px-2 sc-py-0_5 sc-bg-3f3f3f">
                   <p id="buttoniconSizeradiusCount" class="sc-font-thin sc-roboto sc-universal sc-font-size-11">0px</p>
                   <div class="sc-flex sc-flex-col sc-items-center sc-gap-1">
                       <span id="buttoniconSizeIncrease" class="sc-arrow-placeholder"></span>
@@ -97,14 +97,14 @@ export function WidgetButtonAdvanceSection(){
                   <p class="sc-roboto sc-font-thin sc-universal  sc-font-size-12 sc-font-thin sc-text-gray-300"> Center
                   </p>
                   <div id="icon-size-reset"
-                     class="sc-flex sc-cursor-pointer sc-gradiant-border sc-items-center sc-rounded-15px sc-gap-1 sc-p-1_5  sc-bg sc-bg-454545">
+                     class="sc-flex sc-cursor-pointer sc-gradiant-border sc-items-center sc-rounded-15px sc-gap-1 sc-p-1_5 sc-bg-3f3f3f">
 
                      <img src="https://fatin-webefo.github.io/squareCraft-plugin/public/reset.svg" width="10"
                         alt="reset">
                   </div>
                </div>
                <div
-                  class="sc-flex sc-cursor-pointer sc-items-center sc-rounded-15px sc-gap-2 sc-px-2 sc-py-0_5 sc-bg sc-bg-454545">
+                  class="sc-flex sc-cursor-pointer sc-items-center sc-rounded-15px sc-gap-2 sc-px-2 sc-py-0_5 sc-bg-3f3f3f">
                   <p id="buttoniconSizeradiusCount" class="sc-font-thin sc-roboto sc-universal sc-font-size-11">0px</p>
                   <div class="sc-flex sc-flex-col sc-items-center sc-gap-1">
                       <span id="buttoniconSizeIncrease" class="sc-arrow-placeholder"></span>
@@ -125,14 +125,14 @@ export function WidgetButtonAdvanceSection(){
                   <p class="sc-roboto sc-font-thin sc-universal  sc-font-size-12 sc-font-thin sc-text-gray-300"> Exit
                   </p>
                   <div id="icon-size-reset"
-                     class="sc-flex sc-cursor-pointer sc-gradiant-border sc-items-center sc-rounded-15px sc-gap-1 sc-p-1_5  sc-bg sc-bg-454545">
+                     class="sc-flex sc-cursor-pointer sc-gradiant-border sc-items-center sc-rounded-15px sc-gap-1 sc-p-1_5 sc-bg-3f3f3f">
 
                      <img src="https://fatin-webefo.github.io/squareCraft-plugin/public/reset.svg" width="10"
                         alt="reset">
                   </div>
                </div>
                <div
-                  class="sc-flex sc-cursor-pointer sc-items-center sc-rounded-15px sc-gap-2 sc-px-2 sc-py-0_5 sc-bg sc-bg-454545">
+                  class="sc-flex sc-cursor-pointer sc-items-center sc-rounded-15px sc-gap-2 sc-px-2 sc-py-0_5 sc-bg-3f3f3f">
                   <p id="buttoniconSizeradiusCount" class="sc-font-thin sc-roboto sc-universal sc-font-size-11">0px</p>
                   <div class="sc-flex sc-flex-col sc-items-center sc-gap-1">
                       <span id="buttoniconSizeIncrease" class="sc-arrow-placeholder"></span>
@@ -153,14 +153,14 @@ export function WidgetButtonAdvanceSection(){
                   <p class="sc-roboto sc-font-thin sc-universal  sc-font-size-12 sc-font-thin sc-text-gray-300"> Effect Speed
                   </p>
                   <div id="icon-size-reset"
-                     class="sc-flex sc-cursor-pointer sc-gradiant-border sc-items-center sc-rounded-15px sc-gap-1 sc-p-1_5  sc-bg sc-bg-454545">
+                     class="sc-flex sc-cursor-pointer sc-gradiant-border sc-items-center sc-rounded-15px sc-gap-1 sc-p-1_5 sc-bg-3f3f3f">
 
                      <img src="https://fatin-webefo.github.io/squareCraft-plugin/public/reset.svg" width="10"
                         alt="reset">
                   </div>
                </div>
                <div
-                  class="sc-flex sc-cursor-pointer sc-items-center sc-rounded-15px sc-gap-2 sc-px-2 sc-py-0_5 sc-bg sc-bg-454545">
+                  class="sc-flex sc-cursor-pointer sc-items-center sc-rounded-15px sc-gap-2 sc-px-2 sc-py-0_5 sc-bg-3f3f3f">
                   <p id="buttoniconSizeradiusCount" class="sc-font-thin sc-roboto sc-universal sc-font-size-11">0px</p>
                   <div class="sc-flex sc-flex-col sc-items-center sc-gap-1">
                       <span id="buttoniconSizeIncrease" class="sc-arrow-placeholder"></span>
@@ -174,7 +174,32 @@ export function WidgetButtonAdvanceSection(){
                   class="sc-absolute sc-bg-color-EF7C2F sc-w-3 sc-h-3 sc-rounded-full sc-cursor-pointer sc-top-half">
                </div>
             </div>
-           </div></div>
+           </div>
+          
+           <div  class="sc-mt-6 sc-z-[99999]">
+            <p class="sc-universal sc-roboto sc-font-size-12">Effect Animation</p>
+   
+            <div class="sc-flex sc-mt-2 sc-relative sc-items-center sc-z-[99999]">
+               <div  class="sc-bg-3f3f3f sc-relative sc-py-1 sc-rounded-l sc-px-2 sc-w-full ">
+               <p  class="sc-universal sc-roboto sc-font-size-12">None</p>
+               </div>   
+            
+               <div  class="sc-absolute sc-rounded-4px sc-hidden sc-border sc-border-solid sc-h-dropdown sc-scrollBar sc-border-EF7C2F   sc-left-0 sc-top-[35px] sc-z-[99999]">
+                <div class="sc-bg-3f3f3f sc-py-1 sc-font-size-12 sc-px-2 sc-w-16  sc-bg-colo-EF7C2F-hover  sc-cursor-pointer" data-value="none">None</div>
+                <div class="sc-bg-3f3f3f sc-py-1 sc-px-2 sc-font-size-12 sc-w-16 sc-cursor-pointer sc-bg-colo-EF7C2F-hover" data-value="linear">Linear</div>
+                <div class="sc-bg-3f3f3f sc-py-1 sc-px-2 sc-font-size-12 sc-w-16 sc-cursor-pointer sc-bg-colo-EF7C2F-hover" data-value="ease-in">ease-in</div>
+                <div class="sc-bg-3f3f3f sc-py-1 sc-px-2 sc-font-size-12 sc-w-16 sc-cursor-pointer sc-bg-colo-EF7C2F-hover" data-value="ease-out">ease-out</div>
+               </div>
+            
+               <div  class="sc-bg-color-2c2c2c sc-cursor-pointer sc-px-2_5 sc-py-0_5px">
+               <div class="sc-flex sc-flex-col sc-items-center sc-gap-2">
+                  <span class="sc-arrow-placeholder"></span>
+                  <span class="sc-arrow-placeholder sc-rotate-180"></span>
+               </div>
+               </div>
+            </div>
+         </div>
+          </div>
           <div class="sc-flex ">
              <div
                class="sc-bg-3f3f3f sc-relative sc-z-99999 sc-flex sc-border-hover-3d3d3d sc-border sc-border-solid sc-border-3f3f3f sc-cursor-pointer sc-px-2 sc-justify-between sc-py-1 sc-w-full sc-rounded-4px">
