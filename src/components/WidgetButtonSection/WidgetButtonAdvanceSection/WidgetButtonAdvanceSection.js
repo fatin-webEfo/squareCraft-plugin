9dc8ed0ce576923e523cab73a9cc5fe5f8b1b22e
fatin-webEfo <fatin@webefo.com>
2025-06-03 11:07:01 +0600

    add

--- src/components/WidgetButtonSection/WidgetButtonAdvanceSection/WidgetButtonAdvanceSection.js
+++ src/components/WidgetButtonSection/WidgetButtonAdvanceSection/WidgetButtonAdvanceSection.js
@@ -2,6 +2,6 @@ export function WidgetButtonAdvanceSection(){
 
 
     return `
-    <div class="sc-text-sm sc-px-2">Advance tab</div>
+    <div class="sc-text-sm sc-px-2 sc-py-2">Advance tab</div>
     `;
 }
