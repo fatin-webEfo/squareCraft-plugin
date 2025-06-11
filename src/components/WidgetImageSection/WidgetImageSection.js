167cc0fab000902ed425e83b3dba09d880ce2701
fatin-webEfo <fatin@webefo.com>
2025-06-11 16:17:35 +0600

    add

--- src/components/WidgetImageSection/WidgetImageSection.js
+++ src/components/WidgetImageSection/WidgetImageSection.js
@@ -303,7 +303,7 @@ export function WidgetImageSection(id) {
  
                 <div class="sc-mt-3 sc-px-2">
                    <div id="overLayButton"
-                      class="sc-bg-3f3f3f sc-relative sc-z-99999 sc-flex sc-border-hover-3d3d3d sc-border sc-border-solid sc-border-3f3f3f sc-cursor-pointer sc-px-2 sc-justify-between sc-py-2  sc-rounded-4px">
+                      class="sc-bg-3f3f3f  sc-relative sc-z-99999 sc-flex sc-border-hover-3d3d3d sc-border sc-border-solid sc-border-3f3f3f sc-cursor-pointer sc-px-2 sc-justify-between sc-py-2  sc-rounded-4px">
                       <h5 class="sc-roboto sc-font-size-14 sc-universal  sc-font-thin">Overlay</h5>
                         <img id="button-shadow-arrow" src="https://fatin-webefo.github.io/squareCraft-plugin/public/arrow.svg"
                      class="sc-rotate-180" alt="">
@@ -478,7 +478,7 @@ export function WidgetImageSection(id) {
  
  
  
-                <div class="sc-px-2">
+                <div class="sc-px-2 sc-mt-3">
                    <div id="shadowButton"
                       class="sc-bg-3f3f3f sc-relative sc-z-99999 sc-flex sc-border-hover-3d3d3d sc-border sc-border-solid sc-border-3f3f3f sc-cursor-pointer sc-px-2 sc-justify-between sc-py-2  sc-rounded-4px">
                       <h5 class="sc-roboto sc-font-size-14 sc-universal  sc-font-thin">Shadow</h5>
