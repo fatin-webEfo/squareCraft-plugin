a523481d5e5c1c4672c0a66721645dad9fa0347c
fatin-webEfo <fatin@webefo.com>
2025-06-11 17:54:55 +0600

    add

--- src/components/WidgetImageSection/WidgetImageHoverState/WidgetImageHoverState.js
+++ src/components/WidgetImageSection/WidgetImageHoverState/WidgetImageHoverState.js
@@ -1,6 +1,6 @@
 export function WidgetImageHoverState() {
   return `
-    <div id="image-hover-state" class="sc-huidden">
+    <div id="image-hover-state" class="sc-huidden sc-mt-2 sc-px-2">
      Image hover state
     </div>
     `;
