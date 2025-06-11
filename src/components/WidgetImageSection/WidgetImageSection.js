b0855295a7c1e604a17ad9f6ff2ea7fef63cccc6
fatin-webEfo <fatin@webefo.com>
2025-06-11 11:37:52 +0600

    add

--- src/components/WidgetImageSection/WidgetImageSection.js
+++ src/components/WidgetImageSection/WidgetImageSection.js
@@ -10,7 +10,7 @@ export function WidgetImageSection(id) {
                    <img src="https://fatin-webefo.github.io/squareCraft-plugin/public/arrow.svg" alt="">
                 </div>
                 <div class="sc-h-1px sc-bg-3f3f3f"></div>
-                <div class="sc-flex sc-px-2   sc-items-center sc-justify-between">
+                <div class="sc-flex sc-px-2 sc-mt-2  sc-items-center sc-justify-between">
                    <div class="sc-flex sc-gap-2 sc-items-center">
                       <div class="toggle-container" id="toggleSwitch">
                          <div class="toggle-bullet"></div>
@@ -23,7 +23,7 @@ export function WidgetImageSection(id) {
                       <img src="https://fatin-webefo.github.io/squareCraft-plugin/public/reset.svg" alt="reset">
                    </div>
                 </div>
-                <div class="sc-h-1px  sc-bg-3f3f3f"></div>
+                <div class="sc-h-1px sc-mt-2 sc-bg-3f3f3f"></div>
                 <div class="sc-mt-3">
                    <div class="sc-flex sc-roboto sc-px-2  sc-items-center sc-justify-between sc-gap-2">
                       <div
