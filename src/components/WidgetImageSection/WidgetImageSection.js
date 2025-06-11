bd46ee4e7c88657cfdf8072662eb3d191bfe6d9f
fatin-webEfo <fatin@webefo.com>
2025-06-11 14:30:20 +0600

    add

--- src/components/WidgetImageSection/WidgetImageSection.js
+++ src/components/WidgetImageSection/WidgetImageSection.js
@@ -256,7 +256,7 @@ export function WidgetImageSection(id) {
  
  
  
-                      <div style="padding: 2px 0px; margin-top: 10px; margin-left: 70px;"
+                      <div style="padding: 2px 0px; margin-top: 10px; margin-left: 65px;"
                          class="sc-bg-3f3f3f sc-flex sc-col-span-11 sc-text-sm sc-font-thin   sc-border sc-border-solid sc-border-3f3f3f   sc-justify-center  sc-px-1 sc-rounded-4px">
  
    
