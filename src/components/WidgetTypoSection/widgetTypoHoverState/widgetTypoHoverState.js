d4d4b9ed500b3de282adcb99cde1708e576112a3
fatin-webEfo <fatin@webefo.com>
2025-06-15 18:12:24 +0600

    add

--- src/components/WidgetTypoSection/widgetTypoHoverState/widgetTypoHoverState.js
+++ src/components/WidgetTypoSection/widgetTypoHoverState/widgetTypoHoverState.js
@@ -11,25 +11,85 @@ export function widgetTypoHoverState() {
         <img src="https://fatin-webefo.github.io/squareCraft-plugin/public/eye.svg" width="12px" />
       </div>
       <div>
-        ${["heading1", "heading2", "heading3", "heading4", "paragraph1", "paragraph2", "paragraph3"]
-          .map((id, i) => {
-            const label = id.includes("heading") ? `Heading-${id.slice(-1)}` : `Paragraph-${id.slice(-1)}`;
-            const borderClass = i === 0 ? "sc-border-hover-EF7C2F" : "sc-border-hover-3d3d3d";
-            return `
-              <div id="${id}HoverPart" class="sc-hidden">
-                <div class="sc-flex sc-mt-2 sc-px-2">
-                  <div id="${id}Hover"
-                    class="sc-bg-3f3f3f sc-relative sc-z-99999 sc-flex ${borderClass} sc-border sc-border-solid sc-border-3f3f3f sc-cursor-pointer sc-px-2 sc-justify-between sc-py-1 sc-w-full sc-rounded-4px">
-                    ${i === 0 ? '<div class="sc-active-bar sc-rounded-l"></div>' : ""}
-                    <p class="sc-roboto sc-universal sc-font-size-14">${label}</p>
-                    <img id="${id}HoverArrow" src="https://fatin-webefo.github.io/squareCraft-plugin/public/arrow.svg" class="sc-rotate-180" alt="">
-                  </div>
-                </div>
-                ${createHeadingDropdown(`${id}HoverDropdown`, fontSizes, LetterSpacing)}
-              </div>
-            `;
-          })
-          .join("")}
+
+        <div id="heading1HoverPart" class="sc-hidden sc-mb-2">
+          <div class="sc-flex sc-mt-2 sc-px-2">
+            <div id="heading1Hover"
+              class="sc-bg-3f3f3f sc-relative sc-z-99999 sc-flex sc-border-hover-EF7C2F sc-border sc-border-solid sc-border-3f3f3f sc-cursor-pointer sc-px-2 sc-justify-between sc-py-1 sc-w-full sc-rounded-4px">
+              <div class="sc-active-bar sc-rounded-l"></div>
+              <p class="sc-roboto sc-universal sc-font-size-14">Heading-1</p>
+              <img id="heading1HoverArrow" src="https://fatin-webefo.github.io/squareCraft-plugin/public/arrow.svg" class="sc-rotate-180" alt="Heading-1">
+            </div>
+          </div>
+          ${createHeadingDropdown("heading1HoverDropdown", fontSizes, LetterSpacing)}
+        </div>
+
+        <div id="heading2HoverPart" class="sc-hidden sc-mb-2">
+          <div class="sc-flex sc-mt-2 sc-px-2">
+            <div id="heading2Hover"
+              class="sc-bg-3f3f3f sc-relative sc-z-99999 sc-flex sc-border-hover-3d3d3d sc-border sc-border-solid sc-border-3f3f3f sc-cursor-pointer sc-px-2 sc-justify-between sc-py-1 sc-w-full sc-rounded-4px">
+              <p class="sc-roboto sc-universal sc-font-size-14">Heading-2</p>
+              <img id="heading2HoverArrow" src="https://fatin-webefo.github.io/squareCraft-plugin/public/arrow.svg" class="sc-rotate-180" alt="Heading-2">
+            </div>
+          </div>
+          ${createHeadingDropdown("heading2HoverDropdown", fontSizes, LetterSpacing)}
+        </div>
+
+        <div id="heading3HoverPart" class="sc-hidden sc-mb-2">
+          <div class="sc-flex sc-mt-2 sc-px-2">
+            <div id="heading3Hover"
+              class="sc-bg-3f3f3f sc-relative sc-z-99999 sc-flex sc-border-hover-3d3d3d sc-border sc-border-solid sc-border-3f3f3f sc-cursor-pointer sc-px-2 sc-justify-between sc-py-1 sc-w-full sc-rounded-4px">
+              <p class="sc-roboto sc-universal sc-font-size-14">Heading-3</p>
+              <img id="heading3HoverArrow" src="https://fatin-webefo.github.io/squareCraft-plugin/public/arrow.svg" class="sc-rotate-180" alt="Heading-3">
+            </div>
+          </div>
+          ${createHeadingDropdown("heading3HoverDropdown", fontSizes, LetterSpacing)}
+        </div>
+
+        <div id="heading4HoverPart" class="sc-hidden sc-mb-2">
+          <div class="sc-flex sc-mt-2 sc-px-2">
+            <div id="heading4Hover"
+              class="sc-bg-3f3f3f sc-relative sc-z-99999 sc-flex sc-border-hover-3d3d3d sc-border sc-border-solid sc-border-3f3f3f sc-cursor-pointer sc-px-2 sc-justify-between sc-py-1 sc-w-full sc-rounded-4px">
+              <p class="sc-roboto sc-universal sc-font-size-14">Heading-4</p>
+              <img id="heading4HoverArrow" src="https://fatin-webefo.github.io/squareCraft-plugin/public/arrow.svg" class="sc-rotate-180" alt="Heading-4">
+            </div>
+          </div>
+          ${createHeadingDropdown("heading4HoverDropdown", fontSizes, LetterSpacing)}
+        </div>
+
+        <div id="paragraph1HoverPart" class="sc-hidden sc-mb-2">
+          <div class="sc-flex sc-mt-2 sc-px-2">
+            <div id="paragraph1Hover"
+              class="sc-bg-3f3f3f sc-relative sc-z-99999 sc-flex sc-border-hover-3d3d3d sc-border sc-border-solid sc-border-3f3f3f sc-cursor-pointer sc-px-2 sc-justify-between sc-py-1 sc-w-full sc-rounded-4px">
+              <p class="sc-roboto sc-universal sc-font-size-14">Paragraph-1</p>
+              <img id="paragraph1HoverArrow" src="https://fatin-webefo.github.io/squareCraft-plugin/public/arrow.svg" class="sc-rotate-180" alt="Paragraph-1">
+            </div>
+          </div>
+          ${createHeadingDropdown("paragraph1HoverDropdown", fontSizes, LetterSpacing)}
+        </div>
+
+        <div id="paragraph2HoverPart" class="sc-hidden sc-mb-2">
+          <div class="sc-flex sc-mt-2 sc-px-2">
+            <div id="paragraph2Hover"
+              class="sc-bg-3f3f3f sc-relative sc-z-99999 sc-flex sc-border-hover-3d3d3d sc-border sc-border-solid sc-border-3f3f3f sc-cursor-pointer sc-px-2 sc-justify-between sc-py-1 sc-w-full sc-rounded-4px">
+              <p class="sc-roboto sc-universal sc-font-size-14">Paragraph-2</p>
+              <img id="paragraph2HoverArrow" src="https://fatin-webefo.github.io/squareCraft-plugin/public/arrow.svg" class="sc-rotate-180" alt="Paragraph-2">
+            </div>
+          </div>
+          ${createHeadingDropdown("paragraph2HoverDropdown", fontSizes, LetterSpacing)}
+        </div>
+
+        <div id="paragraph3HoverPart" class="sc-hidden sc-mb-2">
+          <div class="sc-flex sc-mt-2 sc-px-2">
+            <div id="paragraph3Hover"
+              class="sc-bg-3f3f3f sc-relative sc-z-99999 sc-flex sc-border-hover-3d3d3d sc-border sc-border-solid sc-border-3f3f3f sc-cursor-pointer sc-px-2 sc-justify-between sc-py-1 sc-w-full sc-rounded-4px">
+              <p class="sc-roboto sc-universal sc-font-size-14">Paragraph-3</p>
+              <img id="paragraph3HoverArrow" src="https://fatin-webefo.github.io/squareCraft-plugin/public/arrow.svg" class="sc-rotate-180" alt="Paragraph-3">
+            </div>
+          </div>
+          ${createHeadingDropdown("paragraph3HoverDropdown", fontSizes, LetterSpacing)}
+        </div>
+
       </div>
     </div>
   `;
