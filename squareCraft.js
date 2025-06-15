7de1e4f364add98526e27dfd8a48e583b327dba6
fatin-webEfo <fatin@webefo.com>
2025-06-15 15:06:15 +0600

    add

--- squareCraft.js
+++ squareCraft.js
@@ -375,10 +375,6 @@
         ?.getAttribute("data-page-sections");
       if (!pageId) return;
 
-      if (!token || !userId) {
-        console.warn("Missing authentication data");
-        return;
-      }
 
       try {
         const response = await fetch(
