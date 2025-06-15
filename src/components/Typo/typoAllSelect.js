24213cc76c1b3a96b6a17a4eb72332def99d02d0
fatin-webEfo <fatin@webefo.com>
2025-06-15 15:03:07 +0600

    add

--- src/components/Typo/typoAllSelect.js
+++ src/components/Typo/typoAllSelect.js
@@ -152,36 +152,43 @@ export function typoAllSelect(fontSizes, LetterSpacing) {
             </div>
           </div>
         </div>
-          <div class="sc-flex sc-text-color-white sc-px-1 
-          sc-rounded-4px sc-border sc-border-solid sc-border-585858 
-          sc-items-center ">
-       <div class="sc-Letter-spacing-container sc-flex sc-justify-between sc-items-center sc-flex sc-items-center sc-border 
-             sc-border-solid sc-border-3d3d3d  sc-rounded-4px 
-             ">
-          <input type="number" id="scLineHeightInput" value="15"
-             class="sc-Letter-spacing-input sc-font-light sc-text-sm sc-text-color-white 
-                sc-bg-transparent sc-w-full  sc-py-1px sc-font-light">
-          <div class="sc-mx-auto sc-ml-1 sc-flex sc-items-center sc-justify-center">
-             <img id="scLetterSpacingDropdown" loading="lazy"
-                src="https://i.ibb.co.com/G460VVdR/Vector.png"
-              style="width: 15px;"  class=" sc-px-1 sc-flex sc-items-center sc-justify-center sc-mx-auto sc-cursor-pointer">
+          <div class="sc-flex sc-text-color-white sc-rounded-4px sc-relative sc-border sc-border-solid sc-border-585858 sc-items-center">
+          <div class="sc-flex sc-text-color-white sc-justify-between sc-col-span-4 sc-rounded-4px sc-items-center">
+            <div class="sc-font-size-container sc-roboto sc-universal sc-flex sc-justify-between sc-items-center sc-rounded-4px">
+              
+              <input type="text" id="scButtonLetterSpacingInput" value="0"
+                class="sc-font-size-input sc-font-light sc-z-99999 sc-font-size-12 sc-text-color-white sc-bg-transparent  sc-universal sc-font-light">
+        
+              <div class="sc-v-line"></div>
+        
+              <div class="sc-flex sc-items-center sc-justify-center sc-px-5px">
+                <p class="sc-font-light sc-text-center sc-font-size-12 sc-universal">px</p>
+              </div>
+        
+              <div id="scButtonLetterSpacingSelect"
+                class="sc-bg-3f3f3f sc-flex sc-items-center sc-justify-center sc-cursor-pointer sc-px-1_5 sc-tooltip-target"
+                style="height: 28px;  border-radius: 0px 5px 5px 0px;">
+                <div class="sc-tooltip sc-hidden">
+                  <div class="tooltip-arrow"></div>
+                  Letter spacing
+                </div>
+                <img id="scLetterSpacingDropdown" loading="lazy"
+   src="https://i.ibb.co.com/G460VVdR/Vector.png"
+ style="width: 15px;"  class=" sc-flex sc-items-center sc-justify-center sc-mx-auto sc-cursor-pointer">
+              </div>
+            </div>
+        
+            <div id="scButtonLetterSpacingOptions"
+              class="sc-absolute sc-hidden sc-scrollBar sc-z-99999 sc-border sc-border-solid sc-border-EF7C2F sc-h-44 sc-font-sm sc-bg-3f3f3f sc-w-12 sc-rounded-4px sc-border-585858 sc-mt-1">
+              ${LetterSpacing?.map(
+                (gap) => `
+                <div class="sc-dropdown-item sc-py-1px sc-text-center sc-font-size-12" data-value="${gap}">
+                  ${gap}
+                </div>`
+              ).join("")}
+            </div>
           </div>
-       </div>
-       <div id="scLetterSpacingOptions" class="sc-hidden sc-h-44 sc-font-sm sc-bg-3f3f3f sc-w-20
-             sc-rounded-4px sc-border sc-border-585858 sc-absolute 
-             sc-mt-1">
-          ${LetterSpacing?.map(
-            (gap) => `
-          <div
-             class="sc-dropdown-item sc-py-1px sc-text-center  sc-text-sm"
-             data-value="${gap}">${gap}
-          </div>
-          `
-          ).join("")}
-       </div>
-    </div>
-  
-       </div>
+        </div>
   
        <div
       class="sc-flex sc-col-span-6 sc-mt-2 sc-justify-between  sc-items-center ">
