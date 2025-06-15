8684b6537f9fc21106cbd4539206f9ebe9e4639e
fatin-webEfo <fatin@webefo.com>
2025-06-15 12:50:24 +0600

    add

--- src/components/Typo/typoAllSelect.js
+++ src/components/Typo/typoAllSelect.js
@@ -58,42 +58,16 @@ export function typoAllSelect(fontSizes, LetterSpacing) {
       
        <div
           class="sc-mt-2  sc-grid sc-grid-cols-12 sc-gap-2 ">
-          <div
-             class="sc-flex sc-bg-494949 sc-pl-2 sc-col-span-7  sc-justify-between sc-border sc-border-solid sc-border-585858 sc-rounded-4px sc-items-center ">
-             <div >
-                <div class="sc-px-2">
-                   <select 
-                         id="squareCraftAllFontWeight"
-                         class="sc-font-weight-select"
-                         data-font-weight-select
-                         style="
-                            background: transparent;
-                            border: none;
-                            color: white;
-                            padding: 5px;
-                            width: 100%;
-                            cursor: pointer;
-                            outline: none;
-                            -webkit-appearance: none;
-                            -moz-appearance: none;
-                            appearance: none;
-                            position: relative;
-                            z-index: 1;
-                         "
-                   >
-                         <option value="100">Thin (100)</option>
-                         <option value="200">Extra Light (200)</option>
-                         <option value="300">Light (300)</option>
-                         <option value="400" selected>Regular (400)</option>
-                         <option value="500">Medium (500)</option>
-                         <option value="600">Semi Bold (600)</option>
-                         <option value="700">Bold (700)</option>
-                         <option value="800">Extra Bold (800)</option>
-                         <option value="900">Black (900)</option>
-                   </select>
-                </div>
-             </div>
-          </div>
+           <div id="scButtonFontWeightSelect" class="sc-flex sc-bg-494949 sc-pl-2 sc-col-span-7 sc-cursor-pointer sc-justify-between sc-border sc-border-solid sc-border-585858 sc-rounded-4px sc-items-center">
+           <div>
+           <p id="scButtonFontWeightSelected" class="sc-font-size-12 sc-universal sc-roboto sc-font-light">
+              400
+           </p>
+           </div>
+           <div class="sc-bg-3f3f3f sc-flex sc-items-center sc-justify-center sc-px-2 sc-h-9" >
+           <img class="sc-mx-auto sc-rotate-180 " width="10px" src="https://fatin-webefo.github.io/squareCraft-plugin/public/arrow.svg" alt="">
+           </div>
+        </div>
   
           <div class="sc-col-span-5 sc-px-2 sc-bg-3f3f3f sc-inActiveTab-border sc-flex sc-justify-between sc-items-center  sc-rounded-4px">
              <p id="textcolorHtml" class="sc-text-sm sc-poppins sc-universal">Select</p>
