2ec4d0c2faa7410762630787063d30170c420caf
fatin-webEfo <fatin@webefo.com>
2025-06-16 13:04:46 +0600

    add

--- src/components/WidgetImageSection/WidgetImageNormalState/WidgetImageNormalState.js
+++ src/components/WidgetImageSection/WidgetImageNormalState/WidgetImageNormalState.js
@@ -214,46 +214,28 @@ export function WidgetImageNormalState(){
 
 
 
-         <div style="padding: 2px 0px; margin-top: 10px; margin-left: 150px;"
-            class="sc-bg-3f3f3f sc-flex sc-gap-1 sc-text-sm sc-font-thin   sc-border sc-border-solid sc-border-3f3f3f   sc-justify-center  sc-px-1 sc-rounded-4px">
-
-
-
-            <div
-               class="sc-flex sc-justify-center  sc-cursor-pointer sc-py-4px sc-rounded-4px sc-items-center sc-gap-1 sc-px-2 sc-bg-454545"  id="allradiusBorder">
-               <img src="https://fatin-webefo.github.io/squareCraft-plugin/public/all.svg" loading="lazy"
-                  alt="all-radious">
-               <p class="sc-font-thin sc-roboto sc-universal">All</p>
-            </div>
-
-
-            <div
-               class="sc-flex sc-justify-center  sc-cursor-pointer sc-py-4px sc-rounded-4px  sc-items-center sc-gap-1 sc-px-1" id="topLeftradiusBorder">
-               <img src="https://fatin-webefo.github.io/squareCraft-plugin/public/right.svg" loading="lazy"
-                  alt="top-left-radius">
-            </div>
-
-
-
-            <div
-               class="sc-flex sc-justify-center  sc-cursor-pointer sc-py-4px sc-rounded-4px  sc-items-center sc-gap-1 sc-px-1" id="topRightradiusBorder">
-               <img src="https://fatin-webefo.github.io/squareCraft-plugin/public/bottom.svg" loading="lazy"
-                  alt="top-right-radius">
-            </div>
-
-
-            <div
-               class="sc-flex sc-justify-center  sc-cursor-pointer sc-py-4px sc-rounded-4px  sc-items-center sc-gap-1 sc-px-1" id="bottomRightradiusBorder">
-               <img src="https://fatin-webefo.github.io/squareCraft-plugin/public/left.svg" loading="lazy"
-                  alt="bottom-right-radius">
-            </div>
-
-
-
-            </div>
-         </div>
-
-      </div>
+         <div style="padding: 2px 0px;" class="sc-bg-3f3f3f sc-flex sc-col-span-11 sc-font-size-12 sc-font-thin   sc-border sc-border-solid sc-border-3f3f3f   sc-justify-center  sc-px-1 sc-rounded-4px">
+                        <div id="buttonBorderAll" class="sc-flex sc-px-1_5 sc-justify-center sc-w-full sc-cursor-pointer sc-py-4px sc-rounded-4px sc-items-center sc-gap-1">
+                           <img src="https://fatin-webefo.github.io/squareCraft-plugin/public/all.svg" loading="lazy" alt="all-border">
+                           <p class="sc-font-thin sc-roboto sc-universal ">All</p>
+                        </div>
+                        <div id="buttonBorderTop" class="sc-flex sc-px-1_5 sc-justify-center sc-w-full sc-cursor-pointer sc-py-4px sc-rounded-4px sc-items-center sc-gap-1">
+                           <img src="https://fatin-webefo.github.io/squareCraft-plugin/public/top.svg" loading="lazy" alt="top-border">
+                           <p class="sc-font-thin sc-roboto sc-universal ">Top</p>
+                        </div>
+                        <div id="buttonBorderBottom" class="sc-flex sc-px-1_5 sc-justify-center sc-w-full sc-cursor-pointer sc-py-4px sc-rounded-4px sc-items-center sc-gap-1">
+                           <img src="https://fatin-webefo.github.io/squareCraft-plugin/public/bottom.svg" loading="lazy" alt="bottom-border">
+                           <p class="sc-font-thin sc-roboto sc-universal ">Bottom</p>
+                        </div>
+                        <div id="buttonBorderLeft" class="sc-flex sc-px-1_5 sc-justify-center sc-w-full sc-cursor-pointer sc-py-4px sc-rounded-4px sc-items-center sc-gap-1">
+                           <img src="https://fatin-webefo.github.io/squareCraft-plugin/public/left.svg" loading="lazy" alt="left-border">
+                           <p class="sc-font-thin sc-roboto sc-universal ">Left</p>
+                        </div>
+                        <div id="buttonBorderRight" class="sc-flex sc-px-1_5 sc-justify-center sc-w-full sc-cursor-pointer sc-py-4px sc-rounded-4px sc-items-center sc-gap-1">
+                           <img src="https://fatin-webefo.github.io/squareCraft-plugin/public/right.svg" loading="lazy" alt="right-border">
+                           <p class="sc-font-thin sc-roboto sc-universal ">Right</p>
+                        </div>
+                     </div>
 
 
    <div class="sc-mt-3 sc-px-2">
