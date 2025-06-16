08e9a3383ca5868fa78892a9d5b944b77aefe57e
fatin-webEfo <fatin@webefo.com>
2025-06-16 16:50:52 +0600

    add

--- src/components/WidgetImageSection/WidgetImageNormalState/WidgetImageNormalState.js
+++ src/components/WidgetImageSection/WidgetImageNormalState/WidgetImageNormalState.js
@@ -214,7 +214,7 @@ export function WidgetImageNormalState(){
 
 
 
-          <div style="padding: 2px 0px;" class="sc-bg-3f3f3f sc-flex sc-col-span-11 sc-font-size-12 sc-font-thin   sc-border sc-border-solid sc-border-3f3f3f   sc-justify-center  sc-px-1 sc-rounded-4px">
+          <div style="padding: 2px 0px;" class="sc-bg-3f3f3f sc-flex sc-mt-2 sc-col-span-11 sc-font-size-12 sc-font-thin   sc-border sc-border-solid sc-border-3f3f3f   sc-justify-center  sc-px-1 sc-rounded-4px">
                         <div  class="sc-flex sc-px-1_5 sc-justify-center sc-w-full sc-cursor-pointer sc-py-4px sc-rounded-4px sc-items-center sc-gap-1">
                            <img src="https://fatin-webefo.github.io/squareCraft-plugin/public/all.svg" loading="lazy" alt="all-border">
                            <p class="sc-font-thin sc-roboto sc-universal ">All</p>
