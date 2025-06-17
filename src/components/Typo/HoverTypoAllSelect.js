7ad33266ca22a7ff293c22317e867161e6d6fa43
fatin-webEfo <fatin@webefo.com>
2025-06-17 17:59:41 +0600

    add

--- src/components/Typo/HoverTypoAllSelect.js
+++ src/components/Typo/HoverTypoAllSelect.js
@@ -2,21 +2,7 @@ export function HoverTypoAllSelect(fontSizes, LetterSpacing) {
   return `
         <div
         class="sc-mt-2 sc-relative sc-grid  sc-grid-cols-12 sc-gap-2 ">
-   
-        <div 
-           class="sc-flex sc-bg-494949 sc-h-9 sc-col-span-8 sc-rounded-4px sc-justify-between sc-border sc-border-solid sc-border-585858 sc-items-center">
-           
-           <select class=" sc-text-sm sc-poppins sc-font-light"
-              style="background: transparent; color: white; border: none; outline: none; appearance: none; cursor: pointer; padding: 0 8px;">
-              <option value="" selected disabled hidden>Select Font</option>
-           </select>
-  
-           <div class="sc-bg-3f3f3f sc-px-2"
-              style="height: 27px; padding: 0 3px; pointer-events: none;">
-              <img class="sc-rotate-180 sc-mt-3" width="12px"
-                 src="https://goswami34.github.io/squareCraft-widget/public/arrow.svg" alt="">
-           </div>
-        </div>
+
    
    
    
@@ -71,7 +57,6 @@ export function HoverTypoAllSelect(fontSizes, LetterSpacing) {
               <div >
                  <div class="sc-px-2">
                     <select 
-                          id="squareCraftAllFontWeight"
                           class="sc-font-weight-select"
                           data-font-weight-select
                           style="
@@ -104,8 +89,8 @@ export function HoverTypoAllSelect(fontSizes, LetterSpacing) {
            </div>
    
            <div class="sc-col-span-5 sc-px-2 sc-bg-3f3f3f sc-inActiveTab-border sc-flex sc-justify-between sc-items-center  sc-rounded-4px">
-              <p id="textcolorHtml" class="sc-text-sm sc-poppins sc-universal">Select</p>
-              <div id="textColorPalate" class="sc-square-6 sc-border-colors sc-cursor-pointer"></div>
+              <p  class="sc-text-sm sc-poppins sc-universal">Select</p>
+              <div  class="sc-square-6 sc-border-colors sc-cursor-pointer"></div>
            </div>
           <div>
            
@@ -118,27 +103,27 @@ export function HoverTypoAllSelect(fontSizes, LetterSpacing) {
            <div
               class="sc-flex sc-col-span-5 sc-justify-between  sc-items-center ">
              <div
-                 class="sc-flex sc-items-center  id="squareCraftAllTextAlign">
+                 class="sc-flex sc-items-center  >
                  
-                 <div id="scTextAlignLeft" data-align="left" class="sc-pt-1 sc-pb-1 sc-px-1_5 sc-inActiveTab-border sc-cursor-pointer sc-rounded-l">
+                 <div data-align="left" class="sc-pt-1 sc-pb-1 sc-px-1_5 sc-inActiveTab-border sc-cursor-pointer sc-rounded-l">
    
                     <img 
                        src="https://goswami34.github.io/squareCraft-widget/public/alignment (4).svg"
                        class=" alignment-icon   sc-mx-auto" alt="left">
                  </div>
-                  <div id="scTextAlignCenter" data-align="center" class="sc-pt-1 sc-pb-1 sc-px-1_5 sc-inActiveTab-border sc-cursor-pointer ">
+                  <div  data-align="center" class="sc-pt-1 sc-pb-1 sc-px-1_5 sc-inActiveTab-border sc-cursor-pointer ">
    
                     <img 
                        src="https://goswami34.github.io/squareCraft-widget/public/alignment (3).svg"
                        class=" alignment-icon   sc-mx-auto" alt="center">
                  </div>
-                  <div id="scTextAlignRight" data-align="right" class="sc-pt-1 sc-pb-1 sc-px-1_5 sc-inActiveTab-border sc-cursor-pointer">
+                  <div data-align="right" class="sc-pt-1 sc-pb-1 sc-px-1_5 sc-inActiveTab-border sc-cursor-pointer">
    
                     <img 
                        src="https://goswami34.github.io/squareCraft-widget/public/alignment (2).svg"
                        class=" alignment-icon   sc-mx-auto" alt="right">
                  </div>
-                 <div id="scTextAlignJustify" data-align="justify" class="sc-pt-1 sc-pb-1 sc-px-1_5 sc-inActiveTab-border sc-cursor-pointer sc-rounded-r">
+                 <div  data-align="justify" class="sc-pt-1 sc-pb-1 sc-px-1_5 sc-inActiveTab-border sc-cursor-pointer sc-rounded-r">
    
                     <img 
                        src="https://goswami34.github.io/squareCraft-widget/public/alignment (1).svg"
@@ -155,16 +140,16 @@ export function HoverTypoAllSelect(fontSizes, LetterSpacing) {
               <div class="sc-Letter-spacing-container sc-flex sc-justify-between sc-items-center sc-flex sc-items-center sc-border 
                     sc-border-solid sc-border-3d3d3d  sc-rounded-4px 
                     ">
-                 <input type="number" id="scLetterSpacingInput" value="15"
+                 <input type="number"  value="15"
                     class="sc-Letter-spacing-input sc-font-light sc-text-sm sc-text-color-white 
                        sc-bg-transparent sc-w-full  sc-py-1px sc-font-light">
                  <div class="sc-mx-auto sc-ml-1 sc-flex sc-items-center sc-justify-center">
-                    <img id="scLetterSpacingDropdown" loading="lazy"
+                    <img  loading="lazy"
                        src="https://goswami34.github.io/squareCraft-widget/public/line-spacing.svg"
                      style="width: 12px;"  class=" sc-px-1 sc-flex sc-items-center sc-justify-center sc-mt-1 sc-mx-auto sc-cursor-pointer">
                  </div>
               </div>
-              <div id="scLetterSpacingOptions" class="sc-hidden sc-h-44 sc-font-sm sc-bg-3f3f3f sc-w-20
+              <div  class="sc-hidden sc-h-44 sc-font-sm sc-bg-3f3f3f sc-w-20
                     sc-rounded-4px sc-border sc-border-585858 sc-absolute 
                     sc-mt-1">
                  ${LetterSpacing?.map(
@@ -183,16 +168,16 @@ export function HoverTypoAllSelect(fontSizes, LetterSpacing) {
         <div class="sc-Letter-spacing-container sc-flex sc-justify-between sc-items-center sc-flex sc-items-center sc-border 
               sc-border-solid sc-border-3d3d3d  sc-rounded-4px 
               ">
-           <input type="number" id="scLineHeightInput" value="15"
+           <input type="number"  value="15"
               class="sc-Letter-spacing-input sc-font-light sc-text-sm sc-text-color-white 
                  sc-bg-transparent sc-w-full  sc-py-1px sc-font-light">
            <div class="sc-mx-auto sc-ml-1 sc-flex sc-items-center sc-justify-center">
-              <img id="scLetterSpacingDropdown" loading="lazy"
+              <img  loading="lazy"
                  src="https://i.ibb.co.com/G460VVdR/Vector.png"
                style="width: 15px;"  class=" sc-px-1 sc-flex sc-items-center sc-justify-center sc-mx-auto sc-cursor-pointer">
            </div>
         </div>
-        <div id="scLetterSpacingOptions" class="sc-hidden sc-h-44 sc-font-sm sc-bg-3f3f3f sc-w-20
+        <div  class="sc-hidden sc-h-44 sc-font-sm sc-bg-3f3f3f sc-w-20
               sc-rounded-4px sc-border sc-border-585858 sc-absolute 
               sc-mt-1">
            ${LetterSpacing?.map(
@@ -211,24 +196,24 @@ export function HoverTypoAllSelect(fontSizes, LetterSpacing) {
         <div
        class="sc-flex sc-col-span-6 sc-mt-2 sc-justify-between  sc-items-center ">
       <div
-          class="sc-flex sc-items-center" id="squareCraftAllTextTransform">
+          class="sc-flex sc-items-center" >
           
-          <div id="scTextTransformUppercase" 
+          <div 
                     data-text-transform="uppercase"  class="sc-pt-1 sc-pb-1 sc-px-1_5 sc-inActiveTab-border sc-cursor-pointer sc-rounded-l">
   
              <p class="sc-universal sc-poppins sc-text-sm squareCraft-text-transform">AG</p>
           </div>
-           <div id="scTextTransformLowercase" 
+           <div 
                     data-text-transform="lowercase"  data-align="center" class="sc-pt-1 sc-pb-1 sc-px-1_5 sc-inActiveTab-border sc-cursor-pointer ">
   
             <p class="sc-universal sc-poppins sc-text-sm squareCraft-text-transform">ag</p>
           </div>
-           <div id="scTextTransformCapitalize" 
+           <div 
                     data-text-transform="capitalize"  data-align="right" class="sc-pt-1 sc-pb-1 sc-px-1_5 sc-inActiveTab-border sc-cursor-pointer">
   
             <p class="sc-universal sc-poppins sc-text-sm squareCraft-text-transform">Ag</p>
           </div>
-          <div id="scTextTransformNone" 
+          <div 
                     data-text-transform="small-caps"  class="sc-pt-1 sc-pb-1 sc-px-1_5 sc-inActiveTab-border sc-cursor-pointer sc-rounded-r">
             <p class="sc-universal sc-poppins sc-text-sm squareCraft-text-transform">AG</p>
           </div>
@@ -238,8 +223,8 @@ export function HoverTypoAllSelect(fontSizes, LetterSpacing) {
         <div class="sc-mt-4 ">
            <p class="sc-text-xs sc-font-thin sc-mt-4 sc-universal sc-text-gray-300 sc-poppins">Text Highlight</p>
            <div class="sc-py-1 sc-mt-2 sc-bg-3f3f3f sc-inActiveTab-border sc-w-50 sc-flex sc-justify-between sc-items-center sc-px-2 sc-rounded-4px">
-              <p class="sc-text-sm sc-poppins c-font-light sc-universal" id="texHeightlistHtml">Select</p>
-              <div id="texHeightlistPalate" class="sc-square-6 sc-border-colors sc-cursor-pointer"></div>
+              <p class="sc-text-sm sc-poppins c-font-light sc-universal" >Select</p>
+              <div class="sc-square-6 sc-border-colors sc-cursor-pointer"></div>
            </div>
         </div> `;
 }
