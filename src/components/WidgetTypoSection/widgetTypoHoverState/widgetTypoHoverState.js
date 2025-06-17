6761b12781c011a3e09f8b1e0dbd68013ae46bdc
fatin-webEfo <fatin@webefo.com>
2025-06-17 11:41:26 +0600

    add

--- src/components/WidgetTypoSection/widgetTypoHoverState/widgetTypoHoverState.js
+++ src/components/WidgetTypoSection/widgetTypoHoverState/widgetTypoHoverState.js
@@ -1,14 +1,14 @@
-import { createHeadingDropdown } from "https://fatin-webefo.github.io/squareCraft-plugin/generateHeadingDropdown.js";
+  import { createHeadingDropdown } from "https://fatin-webefo.github.io/squareCraft-plugin/generateHeadingDropdown.js";
 
-export function widgetTypoHoverState() {
-  const fontSizes = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13"];
-  const LetterSpacing = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"];
+  export function widgetTypoHoverState() {
+    const fontSizes = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13"];
+    const LetterSpacing = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"];
 
-  return `
-    <div id="typo-hover-state" class="sc-hidden sc-mt-2 sc-px-2">
-      <div class="sc-flex sc-justify-between">
-        <p class=" sc-universal sc-roboto sc-text-gray-300">Hover Text Section</p>
+    return `
+      <div id="typo-hover-state" class="sc-hidden sc-mt-2 sc-px-2">
+        <div class="sc-flex sc-justify-between">
+          <p class=" sc-universal sc-roboto sc-text-gray-300">Hover Text Section</p>
+        </div>
       </div>
-    </div>
-  `;
-}
+    `;
+  }
--- src/components/WidgetTypoSection/widgetTypoNormalState/widgetTypoNormalState.js
+++ src/components/WidgetTypoSection/widgetTypoNormalState/widgetTypoNormalState.js
@@ -2,8 +2,8 @@
 import { createHeadingDropdown } from "https://fatin-webefo.github.io/squareCraft-plugin/generateHeadingDropdown.js";
 
 export function widgetTypoNormalState(){
-    const fontSizes = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13"];
-    const LetterSpacing = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"];
+   const fontSizes = Array.from({ length: 40 }, (_, i) => `${i + 1}`),
+     LetterSpacing = Array.from({ length: 40 }, (_, i) => `${i}`);
     return `
     <div id="typo-normal-state">
        <div class=" sc-mt-2 sc-px-2 sc-flex sc-justify-between">
