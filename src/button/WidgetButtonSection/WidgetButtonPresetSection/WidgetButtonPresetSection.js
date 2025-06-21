5c2a70615603794cae8b501b37f50b396d61eab7
fatin-webEfo <fatin@webefo.com>
2025-06-21 17:49:20 +0600

    add

--- src/button/WidgetButtonSection/WidgetButtonPresetSection/WidgetButtonPresetSection.js
+++ src/button/WidgetButtonSection/WidgetButtonPresetSection/WidgetButtonPresetSection.js
@@ -8,7 +8,7 @@ export function WidgetButtonPresetSection() {
       <div class="sc-text-gray-400 sc-text-xs">Live preview of pre-built styles for typography, image, and buttons.</div>
   
       <div class="sc-grid sc-grid-cols-2 sc-gap-4" id="presetGrid">
-        <div class="preset-card sc-bg-2a2a2a sc-rounded-xl sc-p-3 sc-flex-col sc-gap-2 sc-border sc-border-333333 hover:sc-border-EF7C2F sc-transition-all sc-cursor-pointer" data-aos="fade-up" data-aos-delay="0">
+        <div class="preset-card sc-bg-2a2a2a sc-rounded-xl sc-p-3 sc-flex-col sc-gap-2  sc-transition-all sc-cursor-pointer" data-aos="fade-up" data-aos-delay="0">
           <div class="sc-rounded-lg sc-overflow-hidden sc-bg-black sc-border sc-border-262626" style="width: 100%; height: 160px;">
             <img src="https://fatin-webefo.github.io/squareCraft-plugin/public/preset-demo1.gif" alt="Bold Highlight Text" style="width: 100%; height: 100%; object-fit: contain;">
           </div>
@@ -17,7 +17,7 @@ export function WidgetButtonPresetSection() {
           <button class="sc-bg-EF7C2F sc-text-white sc-rounded-md sc-text-xs sc-py-1 sc-mt-2 sc-w-full sc-font-medium hover:sc-bg-orange-600 transition-all">Use This Preset</button>
         </div>
   
-        <div class="preset-card sc-bg-2a2a2a sc-rounded-xl sc-p-3 sc-flex-col sc-gap-2 sc-border sc-border-333333 hover:sc-border-EF7C2F sc-transition-all sc-cursor-pointer" data-aos="fade-up" data-aos-delay="100">
+        <div class="preset-card sc-bg-2a2a2a sc-rounded-xl sc-p-3 sc-flex-col sc-gap-2  sc-transition-all sc-cursor-pointer" data-aos="fade-up" data-aos-delay="100">
           <div class="sc-rounded-lg sc-overflow-hidden sc-bg-black sc-border sc-border-262626" style="width: 100%; height: 160px;">
             <img src="https://fatin-webefo.github.io/squareCraft-plugin/public/preset-demo2.gif" alt="Minimal Button" style="width: 100%; height: 100%; object-fit: contain;">
           </div>
@@ -26,7 +26,7 @@ export function WidgetButtonPresetSection() {
           <button class="sc-bg-EF7C2F sc-text-white sc-rounded-md sc-text-xs sc-py-1 sc-mt-2 sc-w-full sc-font-medium hover:sc-bg-orange-600 transition-all">Use This Preset</button>
         </div>
   
-        <div class="preset-card sc-bg-2a2a2a sc-rounded-xl sc-p-3 sc-flex-col sc-gap-2 sc-border sc-border-333333 hover:sc-border-EF7C2F sc-transition-all sc-cursor-pointer" data-aos="fade-up" data-aos-delay="200">
+        <div class="preset-card sc-bg-2a2a2a sc-rounded-xl sc-p-3 sc-flex-col sc-gap-2  sc-transition-all sc-cursor-pointer" data-aos="fade-up" data-aos-delay="200">
           <div class="sc-rounded-lg sc-overflow-hidden sc-bg-black sc-border sc-border-262626" style="width: 100%; height: 160px;">
             <img src="https://fatin-webefo.github.io/squareCraft-plugin/public/preset-demo3.gif" alt="Shadow Pop" style="width: 100%; height: 100%; object-fit: contain;">
           </div>
@@ -35,7 +35,7 @@ export function WidgetButtonPresetSection() {
           <button class="sc-bg-EF7C2F sc-text-white sc-rounded-md sc-text-xs sc-py-1 sc-mt-2 sc-w-full sc-font-medium hover:sc-bg-orange-600 transition-all">Use This Preset</button>
         </div>
   
-        <div class="preset-card sc-bg-2a2a2a sc-rounded-xl sc-p-3 sc-flex-col sc-gap-2 sc-border sc-border-333333 hover:sc-border-EF7C2F sc-transition-all sc-cursor-pointer" data-aos="fade-up" data-aos-delay="300">
+        <div class="preset-card sc-bg-2a2a2a sc-rounded-xl sc-p-3 sc-flex-col sc-gap-2  sc-transition-all sc-cursor-pointer" data-aos="fade-up" data-aos-delay="300">
           <div class="sc-rounded-lg sc-overflow-hidden sc-bg-black sc-border sc-border-262626" style="width: 100%; height: 160px;">
             <img src="https://fatin-webefo.github.io/squareCraft-plugin/public/preset-demo4.gif" alt="Slide Hover" style="width: 100%; height: 100%; object-fit: contain;">
           </div>
