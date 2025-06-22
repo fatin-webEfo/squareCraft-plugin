a46a87f813d31a75c0220bf912a76b3ff2f35675
fatin-webEfo <fatin@webefo.com>
2025-06-22 14:28:04 +0600

    add

--- ToolbarIconHtml.js
+++ ToolbarIconHtml.js
@@ -1,6 +1,6 @@
 export function ToolbarIconHtml(){
     return `
-      <div class="sc-p-2  z-index-high sc-text-color-white sc-border sc-border-solid sc-border-3d3d3d sc-bg-color-2c2c2c sc-rounded-15px sc-w-300px"> 
+ 
    <div id="sc-grabbing" class="sc-cursor-grabbing sc-w-full">
       <div class="sc-flex sc-roboto sc-universal sc-items-center sc-justify-between">
         <img class="sc-cursor-grabbing sc-universal" src="https://i.ibb.co.com/pry1mVGD/Group-28-1.png" width="140px" />
@@ -32,7 +32,6 @@ export function ToolbarIconHtml(){
           Reset
         </div>
       </div>
-    </div>
     </div>
     `;
 }
