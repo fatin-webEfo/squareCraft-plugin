0a11a71159ac6c5140c59d82fc82bdffdf55ab39
fatin-webEfo <fatin@webefo.com>
2025-06-22 15:55:09 +0600

    add

--- ToolbarIconHtml.js
+++ ToolbarIconHtml.js
@@ -19,6 +19,32 @@ export function ToolbarIconHtml(section) {
     <div class="sc-border-t sc-border-solid sc-relative sc-border-color-494949 sc-w-full">
       <div class="sc-absolute sc-top-0 sc-left-0 sc-bg-color-EF7C2F sc-w-16 sc-h-1px sc-tab-active-indicator"></div>
     </div>
+    <div class="sc-rounded-4px sc-h-350 sc-scrollBar sc-mt-6 sc-border sc-border-solid sc-border-EF7C2F sc-bg-color-3d3d3d">
+      <div class="sc-flex ">
+           <div class="sc-bg-3f3f3f sc-relative sc-z-99999 sc-flex sc-border-hover-3d3d3d sc-border sc-border-solid sc-border-3f3f3f sc-cursor-pointer sc-px-2 sc-justify-between sc-py-1 sc-w-full sc-rounded-4px">
+                
+                  <p class="sc-roboto sc-font-size-14 sc-universal ">Structure</p>
+                  <img id="button-font-arrow" src="https://fatin-webefo.github.io/squareCraft-plugin/public/arrow.svg"
+                     class="" alt="">
+            </div>
+        </div>
+      <div class="sc-flex ">
+           <div class="sc-bg-3f3f3f sc-relative sc-z-99999 sc-flex sc-border-hover-3d3d3d sc-border sc-border-solid sc-border-3f3f3f sc-cursor-pointer sc-px-2 sc-justify-between sc-py-1 sc-w-full sc-rounded-4px">
+                
+                  <p class="sc-roboto sc-font-size-14 sc-universal ">Background</p>
+                  <img id="button-font-arrow" src="https://fatin-webefo.github.io/squareCraft-plugin/public/arrow.svg"
+                     class="" alt="">
+            </div>
+        </div>
+      <div class="sc-flex ">
+           <div class="sc-bg-3f3f3f sc-relative sc-z-99999 sc-flex sc-border-hover-3d3d3d sc-border sc-border-solid sc-border-3f3f3f sc-cursor-pointer sc-px-2 sc-justify-between sc-py-1 sc-w-full sc-rounded-4px">
+                
+                  <p class="sc-roboto sc-font-size-14 sc-universal ">Border</p>
+                  <img id="button-font-arrow" src="https://fatin-webefo.github.io/squareCraft-plugin/public/arrow.svg"
+                     class="" alt="">
+            </div>
+        </div>
+    </div>
     <div class="sc-mt-3">
       <div class="sc-flex sc-items-center sc-justify-between sc-gap-2">
         <div class="sc-cursor-pointer sc-roboto sc-bg-color-EF7C2F sc-w-full sc-font-light sc-flex sc-items-center sc-font-size-12 sc-py-4px sc-rounded-4px sc-text-color-white sc-justify-center">
