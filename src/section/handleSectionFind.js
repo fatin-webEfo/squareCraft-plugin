2acbc4a4b8b35f33941e11034053e4501dab3ef2
fatin-webEfo <fatin@webefo.com>
2025-06-23 10:37:24 +0600

    add

--- src/section/handleSectionFind.js
+++ src/section/handleSectionFind.js
@@ -10,8 +10,44 @@ export function handleSectionFind() {
 
     const blocks = Array.from(blockEls).map((el) => {
       const blockId = el.id;
-      const type = detectBlockElementTypePure(el);
-      return { blockId, type };
+      let foundType = detectBlockElementTypePure(el);
+      const tag = el.tagName?.toLowerCase();
+      const cls = el.classList;
+
+      if (!foundType && (tag === "a" || tag === "button")) {
+        const iconImg = el.querySelector("img");
+        if (
+          !iconImg ||
+          (iconImg && iconImg.classList.contains("sqscraft-button-icon"))
+        ) {
+          foundType = "button";
+
+          let currentButtonType;
+          if (cls.contains("sqs-button-element--primary"))
+            currentButtonType = "Primary Button";
+          else if (cls.contains("sqs-button-element--secondary"))
+            currentButtonType = "Secondary Button";
+          else if (cls.contains("sqs-button-element--tertiary"))
+            currentButtonType = "Tertiary Button";
+          else currentButtonType = "Button";
+
+          const buttonTypeEl = document.getElementById("buttonTypeDisplay");
+          if (buttonTypeEl) {
+            buttonTypeEl.textContent = currentButtonType;
+          }
+        }
+      }
+
+      if (
+        !foundType &&
+        tag === "img" &&
+        el.closest(".sqs-image-content") &&
+        el.closest(".fluid-image-editor-wrapper")
+      ) {
+        foundType = "image";
+      }
+
+      return { blockId, type: foundType };
     });
 
     let sectionType = "block";
@@ -41,6 +77,5 @@ export function handleSectionFind() {
   });
 
   console.log("🧩 handleSectionFind() Output:", results);
-
   return results;
 }
