c982e249518cecfa6b8d49431526db4209087bd1
fatin-webEfo <fatin@webefo.com>
2025-06-23 14:48:50 +0600

    add

--- src/button/initButtonAdvanceStyles/initButtonAdvanceStyles.js
+++ src/button/initButtonAdvanceStyles/initButtonAdvanceStyles.js
@@ -3,6 +3,7 @@ import { gsap } from "https://cdn.jsdelivr.net/npm/gsap@3.13.0/dist/gsap.min.js"
 import { ScrollTrigger } from "https://cdn.jsdelivr.net/npm/gsap@3.13.0/dist/ScrollTrigger.min.js";
 
 
+
 gsap.registerPlugin(ScrollTrigger);
 
 function getValidButton(el) {
