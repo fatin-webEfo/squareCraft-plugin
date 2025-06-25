9d807ee60be6081b207732307287a1324c164016
fatin-Webefo <fatin@webefo.com>
2025-06-25 10:52:08 +0600

    add

--- NavbarIconHtml.js
+++ NavbarIconHtml.js
@@ -5,19 +5,19 @@ export function NavbarIconHtml() {
       <div id="icon-options">
   <div style="padding: 12px 16px; color: white; font-size: 14px; display: flex; align-items: center; justify-content: space-between;">
     <span style="font-size: 18px; font-weight: 400;">SquareCraft</span>
-   
+     <div id="viewport-sections" style=" display: flex; align-items: center; gap: 10px; justify-content: center;">
+      <img style="cursor:pointer" id="mobile-viewport" class="sc-cursor-pointer" src="https://fatin-webefo.github.io/squareCraft-plugin/public/viewport/mobile.svg" style="width: 18px;">
+      <img style="cursor:pointer" id="tab-viewport" class="sc-cursor-pointer" src="https://fatin-webefo.github.io/squareCraft-plugin/public/viewport/tab.svg" style="width: 18px;">
+      <img style="cursor:pointer" id="laptop-viewport" class="sc-cursor-pointer" src="https://fatin-webefo.github.io/squareCraft-plugin/public/viewport/laptop.svg" style="width: 18px;">
+      <img style="cursor:pointer" id="dekstop-viewport" class="sc-cursor-pointer" src="https://fatin-webefo.github.io/squareCraft-plugin/public/viewport/monitor.svg" style="width: 18px;">
+    </div>
   </div>
   <div style="background: #EF7C2F; color: white; font-size: 12px; padding: 6px 12px; text-align: center;">
     Your free trial expires in 0 days. <span style="text-decoration: underline; cursor: pointer;">Click here to upgrade.</span>
   </div>
   
   <div style="display: grid; grid-template-columns: repeat(3, 1fr); gap: 1px; background: #2c2c2c; padding: 1px;">
-   <div id="viewport-sections" style=" display: flex; align-items: center; gap: 10px; justify-content: center;">
-      <img style="cursor:pointer" id="mobile-viewport" class="sc-cursor-pointer" src="https://fatin-webefo.github.io/squareCraft-plugin/public/viewport/mobile.svg" style="width: 18px;">
-      <img style="cursor:pointer" id="tab-viewport" class="sc-cursor-pointer" src="https://fatin-webefo.github.io/squareCraft-plugin/public/viewport/tab.svg" style="width: 18px;">
-      <img style="cursor:pointer" id="laptop-viewport" class="sc-cursor-pointer" src="https://fatin-webefo.github.io/squareCraft-plugin/public/viewport/laptop.svg" style="width: 18px;">
-      <img style="cursor:pointer" id="dekstop-viewport" class="sc-cursor-pointer" src="https://fatin-webefo.github.io/squareCraft-plugin/public/viewport/monitor.svg" style="width: 18px;">
-    </div>
+ 
     <div style="background:#1f1f1f; color:white; font-size:12px; text-align:center; padding: 16px 0; cursor:pointer; transition:background 0.2s;
       display: flex; flex-direction: column; align-items: center; justify-content: center; gap: 6px;"
       onmouseenter="this.style.background='#3a3a3a'"
