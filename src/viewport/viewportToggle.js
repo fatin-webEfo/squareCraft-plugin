1d90fbf715b6ddf7898cf5512d9039fa8cdff42c
fatin-Webefo <fatin@webefo.com>
2025-06-26 10:17:39 +0600

    add

--- src/viewport/viewportToggle.js
+++ src/viewport/viewportToggle.js
@@ -4,7 +4,7 @@ export function viewportToggle(attempt = 0) {
   const views = {
     mobile: { id: "mobile-viewport", frameWidth: "428px" },
     tablet: { id: "tab-viewport", frameWidth: "720px" },
-    laptop: { id: "laptop-viewport", frameWidth: "1200px" },
+    laptop: { id: "laptop-viewport", frameWidth: "1280px" },
     desktop: { id: "dekstop-viewport", frameWidth: "1440px" }, 
   };
 
