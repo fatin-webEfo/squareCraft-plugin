dbb3f271597123ca7f61bf5336049975b08436e5
fatin-Webefo <fatin@webefo.com>
2025-06-29 17:28:38 +0600

    add

--- src/button/WidgetButtonSection/WidgetButtonAdvanceSection/WidgetButtonAdvanceSection.js
+++ src/button/WidgetButtonSection/WidgetButtonAdvanceSection/WidgetButtonAdvanceSection.js
@@ -364,7 +364,7 @@ export function WidgetButtonAdvanceSection(){
           </div>
          
         </div>
-        <div class="sc-mt-2 sc-px-2"><div
+        <div class="sc-mt-2"><div
   id="button-advance-horizontal"
   class="sc-bg-3f3f3f sc-relative sc-z-99999 sc-flex sc-border-hover-3d3d3d sc-border sc-border-solid sc-border-3f3f3f sc-cursor-pointer sc-px-2 sc-justify-between sc-py-1 sc-rounded-4px"
 >
@@ -727,7 +727,7 @@ export function WidgetButtonAdvanceSection(){
   </div>
 </div></div>
         <div
-          class="sc-flex sc-p-2 sc-cursor-pointer sc-items-center sc-justify-between sc-mt-2">
+          class="sc-flex sc-p-2 sc-cursor-pointer sc-items-center sc-justify-between">
           <div class="sc-flex sc-gap-2 sc-items-center">
             <img
               loading="lazy"
