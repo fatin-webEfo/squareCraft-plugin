629198fe022fbbc0a144cbcc7c18430334b28cb7
fatin-Webefo <fatin@webefo.com>
2025-07-01 16:35:35 +0600

    add

--- src/button/buttonAdvanceSyncCustomTimelineArrow/buttonAdvanceSyncCustomTimelineArrow.js
+++ src/button/buttonAdvanceSyncCustomTimelineArrow/buttonAdvanceSyncCustomTimelineArrow.js
@@ -599,10 +599,11 @@ export function scalebuttonAdvanceSyncCustomTimelineArrow(selectedElement) {
       gsap.to(btn, {
         duration: 0.3,
         ease: transition.ease,
-        transform: `translateY(${finalY.toFixed(2)}vh)`,
+        transform: `scale(${Math.max(0.01, 1 + finalY / 100)})`,
       });
       lastY = finalY;
     }
+    
   }
 
   function trackLoop(arrow, border, startBullet, endBullet, dropdown) {
