d0d10d6efdf9e16033fb0236eb1627cfacbaef24
fatin-Webefo <fatin@webefo.com>
2025-07-01 15:46:25 +0600

    add

--- src/button/buttonAdvanceSyncCustomTimelineArrow/buttonAdvanceSyncCustomTimelineArrow.js
+++ src/button/buttonAdvanceSyncCustomTimelineArrow/buttonAdvanceSyncCustomTimelineArrow.js
@@ -390,7 +390,6 @@ export function opacitybuttonAdvanceSyncCustomTimelineArrow(selectedElement) {
     const exitY = getVHFromCSSVar("--sc-opacity-scroll-exit");
     if (entryY === 0 && centerY === 0 && exitY === 0 && !apply) {
       gsap.set(btn, { opacity: 1 });
-      return;
     }
     let y = 0;
     let apply = false;
