c88bc5973053c5e1fb7f4adda045f35b342b00ba
fatin-Webefo <fatin@webefo.com>
2025-07-07 11:46:31 +0600

    add

---
+++ src/button/initButtonStructureGapTypeToggle/initButtonStructureGapTypeToggle.js
@@ -0,0 +1,43 @@
+export function initButtonStructureGapTypeToggle() {
+  const marginIds = [
+    "button-advance-margin-gap-all",
+    "button-advance-margin-gap-top",
+    "button-advance-margin-gap-bottom",
+    "button-advance-margin-gap-left",
+    "button-advance-margin-gap-right",
+  ];
+
+  const paddingIds = [
+    "button-advance-padding-gap-all",
+    "button-advance-padding-gap-top",
+    "button-advance-padding-gap-bottom",
+    "button-advance-padding-gap-left",
+    "button-advance-padding-gap-right",
+  ];
+
+  marginIds.forEach((id) => {
+    const el = document.getElementById(id);
+    if (!el) return;
+
+    el.addEventListener("click", () => {
+      marginIds.forEach((btnId) => {
+        const btn = document.getElementById(btnId);
+        if (btn) btn.classList.remove("sc-bg-454545");
+      });
+      el.classList.add("sc-bg-454545");
+    });
+  });
+
+  paddingIds.forEach((id) => {
+    const el = document.getElementById(id);
+    if (!el) return;
+
+    el.addEventListener("click", () => {
+      paddingIds.forEach((btnId) => {
+        const btn = document.getElementById(btnId);
+        if (btn) btn.classList.remove("sc-bg-454545");
+      });
+      el.classList.add("sc-bg-454545");
+    });
+  });
+}
