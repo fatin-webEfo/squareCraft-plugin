0faf6298347fd6048e515580a5ebaee2eb807000
fatin-Webefo <fatin@webefo.com>
2025-07-10 12:11:53 +0600

    add

---
+++ src/button/WidgetButtonPresetTabControls/WidgetButtonPresetTabControls.js
@@ -0,0 +1,39 @@
+export function WidgetButtonPresetTabControls() {
+  const buttonIds = ["button-my-preset-tab", "button-preset-library-tab"];
+  const sectionIds = [
+    "button-my-preset-section",
+    "button-preset-library-section",
+  ];
+
+  buttonIds.forEach((btnId, index) => {
+    const button = document.getElementById(btnId);
+    const section = document.getElementById(sectionIds[index]);
+
+    if (!button || !section) return;
+
+    button.addEventListener("click", () => {
+      buttonIds.forEach((otherBtnId, i) => {
+        const otherButton = document.getElementById(otherBtnId);
+        const otherSection = document.getElementById(sectionIds[i]);
+
+        if (!otherButton || !otherSection) return;
+
+        if (i === index) {
+          otherButton.classList.add(
+            "sc-bg-color-EF7C2F",
+            "sc-text-color-white"
+          );
+          otherButton.classList.remove("sc-bg-3f3f3f");
+          otherSection.classList.remove("sc-hidden");
+        } else {
+          otherButton.classList.remove(
+            "sc-bg-color-EF7C2F",
+            "sc-text-color-white"
+          );
+          otherButton.classList.add("sc-bg-3f3f3f");
+          otherSection.classList.add("sc-hidden");
+        }
+      });
+    });
+  });
+}
