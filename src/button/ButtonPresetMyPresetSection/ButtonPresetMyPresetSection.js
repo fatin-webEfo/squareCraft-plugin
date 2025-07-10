3414ad9d529f0413f1b8a84bc5a16578fdaa5e2e
fatin-Webefo <fatin@webefo.com>
2025-07-10 11:50:48 +0600

    add

---
+++ src/button/ButtonPresetMyPresetSection/ButtonPresetMyPresetSection.js
@@ -0,0 +1,14 @@
+export function ButtonPresetMyPresetSection() {
+  return `
+    <div id="button-my-preset-section" class="sc-hidden">
+                <p class="sc-universal sc-text-sm sc-text-gray-300 sc-font-thin">Save this block's settings or load a preset you’ve already created.</p>
+
+      <div class="sc-mt-2 sc-border sc-cursor-pointer sc-flex sc-justify-center sc-gap-3 sc-border-585858 sc-py-2 sc-rounded-4px sc-border-solid">
+                  <p class="sc-universal sc-text-sm sc-text-gray-300 sc-font-thin sc-text-center">
+                    Design this block to save as a preset
+                  </p>
+                  <img src="./public/orange-plus.svg" alt="">
+        </div>
+     </div>
+  `;
+}
--- src/button/WidgetButtonSection/WidgetButtonPresetSection/WidgetButtonPresetSection.js
+++ src/button/WidgetButtonSection/WidgetButtonPresetSection/WidgetButtonPresetSection.js
@@ -1,49 +1,18 @@
 export function WidgetButtonPresetSection() {
   return `
-    <div id="presetSection" class="sc-p-4 sc-flex-col sc-gap-4 sc-rounded-2xl sc-bg-1e1e1e sc-text-white sc-shadow-md sc-text-sm">
-      <div class="sc-flex sc-items-center sc-justify-between">
-        <p class="sc-universal sc-font-size-16 sc-roboto">Style Presets</p>
-      </div>
-  
-      <p class="sc-text-gray-400 sc-font-thin sc-universal sc-text-xs">Live preview of pre-built styles for typography, image, and buttons.</p>
-  
-      <div class="sc-grid sc-mt-3 sc-grid-cols-2 sc-gap-4" id="presetGrid">
-        <div class="preset-card sc-bg-2a2a2a sc-rounded-xl sc-p-3 sc-flex-col sc-gap-2  sc-transition-all sc-cursor-pointer" data-aos="fade-up" data-aos-delay="0">
-          <div class="sc-rounded-lg sc-overflow-hidden sc-bg-black sc-border sc-border-EF7C2F" style="width: 100%; height: 160px;">
-            <img src="https://fatin-webefo.github.io/squareCraft-plugin/public/preset-demo1.gif" alt="Bold Highlight Text" style="width: 100%; height: 100%; object-fit: contain;">
-          </div>
-          <div class="sc-font-medium sc-text-xs">Bold Highlight Text</div>
-          <div class="sc-text-gray-400 sc-text-2xs">Text: Bold + Highlight + Center</div>
-          <button class="sc-bg-color-EF7C2F sc-text-color-white sc-rounded-md sc-text-xs sc-py-1 sc-mt-2 sc-w-full sc-font-medium hover:sc-bg-orange-600 transition-all">Use This Preset</button>
-        </div>
-  <div class="sc-mt-6 sc-roboto sc-border-t sc-border-t-dashed sc-border-color-494949  sc-w-full"></div>
-        <div class="preset-card sc-bg-2a2a2a sc-rounded-xl sc-p-3 sc-flex-col sc-gap-2 sc-mt-3 sc-transition-all sc-cursor-pointer" data-aos="fade-up" data-aos-delay="100">
-          <div class="sc-rounded-lg sc-overflow-hidden sc-bg-black sc-border sc-border-EF7C2F" style="width: 100%; height: 160px;">
-            <img src="https://fatin-webefo.github.io/squareCraft-plugin/public/preset-demo2.gif" alt="Minimal Button" style="width: 100%; height: 100%; object-fit: contain;">
-          </div>
-          <div class="sc-font-medium sc-text-xs">Minimal Button</div>
-          <div class="sc-text-gray-400 sc-text-2xs">Button: No border + Thin font</div>
-          <button class="sc-bg-color-EF7C2F sc-text-color-white sc-rounded-md sc-text-xs sc-py-1 sc-mt-2 sc-w-full sc-font-medium hover:sc-bg-orange-600 transition-all">Use This Preset</button>
-        </div>
-  <div class="sc-mt-6 sc-roboto sc-border-t sc-border-t-dashed sc-border-color-494949  sc-w-full"></div>
-        <div class="preset-card sc-bg-2a2a2a sc-rounded-xl sc-p-3 sc-flex-col sc-gap-2 sc-mt-3 sc-transition-all sc-cursor-pointer" data-aos="fade-up" data-aos-delay="200">
-          <div class="sc-rounded-lg sc-overflow-hidden sc-bg-black sc-border sc-border-EF7C2F" style="width: 100%; height: 160px;">
-            <img src="https://fatin-webefo.github.io/squareCraft-plugin/public/preset-demo3.gif" alt="Shadow Pop" style="width: 100%; height: 100%; object-fit: contain;">
-          </div>
-          <div class="sc-font-medium sc-text-xs">Shadow Pop</div>
-          <div class="sc-text-gray-400 sc-text-2xs">Button: Shadow + Hover Grow</div>
-          <button class="sc-bg-color-EF7C2F sc-text-color-white sc-rounded-md sc-text-xs sc-py-1 sc-mt-2 sc-w-full sc-font-medium hover:sc-bg-orange-600 transition-all">Use This Preset</button>
-        </div>
-  <div class="sc-mt-6 sc-roboto sc-border-t sc-border-t-dashed sc-border-color-494949  sc-w-full"></div>
-        <div class="preset-card sc-bg-2a2a2a sc-rounded-xl sc-p-3 sc-flex-col sc-gap-2 sc-mt-3 sc-transition-all sc-cursor-pointer" data-aos="fade-up" data-aos-delay="300">
-          <div class="sc-rounded-lg sc-overflow-hidden sc-bg-black sc-border sc-border-EF7C2F" style="width: 100%; height: 160px;">
-            <img src="https://fatin-webefo.github.io/squareCraft-plugin/public/preset-demo4.gif" alt="Slide Hover" style="width: 100%; height: 100%; object-fit: contain;">
-          </div>
-          <div class="sc-font-medium sc-text-xs">Slide Hover</div>
-          <div class="sc-text-gray-400 sc-text-2xs">Text: Slide left effect on hover</div>
-          <button class="sc-bg-color-EF7C2F sc-text-color-white sc-rounded-md sc-text-xs sc-py-1 sc-mt-2 sc-w-full sc-font-medium hover:sc-bg-orange-600 transition-all">Use This Preset</button>
-        </div>
-      </div>
+   <div id="button-presetSection">
+            <div class="sc-flex sc-roboto sc-p-2 sc-items-center sc-justify-between sc-gap-2">
+              <div id="button-my-preset-tab" class="sc-cursor-pointer sc-bg-color-EF7C2F sc-w-full sc-font-light sc-flex sc-items-center sc-font-size-12 sc-py-1 sc-rounded-4px sc-text-color-white sc-justify-center">
+                My Preset
+              </div>
+              <div id="button-preset-library-tab" class="sc-cursor-pointer sc-bg-3f3f3f sc-w-full sc-text-color-white sc-font-light sc-flex sc-font-size-12 sc-py-1 sc-rounded-4px sc-items-center sc-justify-center">
+                Preset Library
+              </div>
+            </div>
+
+            <div class="sc-px-2">
+             <!-- preset sections -->
+            </div>
     </div>
   
 
