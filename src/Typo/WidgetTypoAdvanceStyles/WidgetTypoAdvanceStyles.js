b563c8fefbedfb18197efedbff4a7b694e5c6eed
fatin-Webefo <fatin@webefo.com>
2025-07-14 15:09:01 +0600

    add

--- src/Typo/WidgetTypoAdvanceStyles/WidgetTypoAdvanceStyles.js
+++ src/Typo/WidgetTypoAdvanceStyles/WidgetTypoAdvanceStyles.js
@@ -172,8 +172,6 @@ export function initTypoAdvanceStyles(getSelectedElement) {
           ).style.left = `${bulletLeft}%`;
         }
         initEffectAnimationDropdownToggle();
-
-        initEffectAnimationDropdownToggle(startBullet, endBullet);
       } else {
         gsap.set(bullet, { left: `${val}%`, xPercent: -50 });
         position === "left"
