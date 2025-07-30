eb75808a1e08280c7d1291064d9f7e98e54a85a1
fatin-Webefo <fatin@webefo.com>
2025-07-30 12:53:53 +0600

    add CSS styling and test class injection to SquareCraft plugin

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
+console.log("✅ SquareCraft plugin loaded from Link file section!");
 
-console.log('SquareCraft plugin loaded from Link file section!');
+const styleLink = document.createElement("link");
+styleLink.rel = "stylesheet";
+styleLink.href = "https://fatin-webefo.github.io/squareCraft-plugin/index.css";
+styleLink.type = "text/css";
+styleLink.media = "all";
+document.head.appendChild(styleLink);
 
-// Add your plugin code here
-  const styleLink = document.createElement("link");
-  styleLink.rel = "stylesheet";
-  styleLink.href =
-    "https://fatin-webefo.github.io/squareCraft-plugin/index.css";
-  styleLink.type = "text/css";
-  styleLink.media = "all";
-  document.head.appendChild(styleLink);
+// Inject test class after load
+styleLink.onload = () => {
+  console.log("✅ CSS file loaded and applied.");
+  document.body.classList.add("sc-test");
+};
