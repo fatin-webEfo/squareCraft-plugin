539e819c7c93f34fe05881a4a970c189bebf130a
fatin-Webefo <fatin@webefo.com>
2025-07-30 16:09:12 +0600

    update stylesheet link to include cache-busting query parameter

--- index.js
+++ index.js
@@ -2,13 +2,17 @@ console.log("✅ SquareCraft plugin loaded from Link file section!");
 
 const styleLink = document.createElement("link");
 styleLink.rel = "stylesheet";
-styleLink.href = "https://fatin-webefo.github.io/squareCraft-plugin/index.css";
+styleLink.href =
+  "https://fatin-webefo.github.io/squareCraft-plugin/index.css?v=" + Date.now();
 styleLink.type = "text/css";
 styleLink.media = "all";
 document.head.appendChild(styleLink);
+
 
 // Inject test class after load
 styleLink.onload = () => {
   console.log("✅ CSS file loaded and applied.");
   document.body.classList.add("sc-test");
 };
+
+// index.js
