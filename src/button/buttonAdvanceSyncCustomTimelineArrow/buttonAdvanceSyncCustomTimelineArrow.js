f3be1db48b138c3e16463445b919683dc331e3ce
fatin-Webefo <fatin@webefo.com>
2025-08-28 17:15:49 +0600

    download

--- src/button/buttonAdvanceSyncCustomTimelineArrow/buttonAdvanceSyncCustomTimelineArrow.js
+++ src/button/buttonAdvanceSyncCustomTimelineArrow/buttonAdvanceSyncCustomTimelineArrow.js
@@ -866,6 +866,7 @@ export function blurbuttonAdvanceSyncCustomTimelineArrow(selectedElement) {
   });
 }
 
+
 export function initButtonAdvanceScrollEffectReset(target) {
   const el = typeof target === "function" ? target() : target;
   if (!el) return;
