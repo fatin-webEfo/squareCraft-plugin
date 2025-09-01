077837c70ccbf9e67a191ce9064f512ad9404e70
fatin-Webefo <fatin@webefo.com>
2025-09-01 10:58:06 +0600

    add

--- src/components/Typo/HoverTypoBoldSelect.js
+++ src/components/Typo/HoverTypoBoldSelect.js
@@ -274,7 +274,7 @@ export function HoverTypoBoldSelect(LetterSpacing) {
 
             <div>
               <div id="typo-bold-hover-border-button"
-                 class="sc-bg-3f3f3f sc-mt-3 sc-relative sc-z-9999 sc-flex sc-border-hover-3d3d3d sc-border sc-border-solid sc-border-3f3f3f sc-cursor-pointer sc-px-2 sc-justify-between sc-py-1 sc-rounded-4px">
+                 class="sc-bg-3f3f3f sc-mt-2 sc-relative sc-z-9999 sc-flex sc-border-hover-3d3d3d sc-border sc-border-solid sc-border-3f3f3f sc-cursor-pointer sc-px-2 sc-justify-between sc-py-1 sc-rounded-4px">
                  <p class="sc-roboto  sc-universal sc-font-size-14">Outline</p>
                  <img  src="https://fatin-webefo.github.io/squareCraft-plugin/public/arrow.svg"
                     class="sc-rotate-180" alt="">
@@ -473,7 +473,7 @@ export function HoverTypoBoldSelect(LetterSpacing) {
               </div>
            </div>
            <div id="typo-bold-hover-shadow-button"
-          class="sc-bg-3f3f3f sc-mt-3 sc-relative sc-z-9999 sc-flex sc-border-hover-3d3d3d sc-border sc-border-solid sc-border-3f3f3f sc-cursor-pointer sc-px-2 sc-justify-between sc-py-1 sc-rounded-4px">
+          class="sc-bg-3f3f3f sc-mt-2 sc-relative sc-z-9999 sc-flex sc-border-hover-3d3d3d sc-border sc-border-solid sc-border-3f3f3f sc-cursor-pointer sc-px-2 sc-justify-between sc-py-1 sc-rounded-4px">
           <p class="sc-roboto  sc-universal sc-font-size-14">Shadow</p>
           <img src="https://fatin-webefo.github.io/squareCraft-plugin/public/arrow.svg"
              class="sc-rotate-180" alt="">
@@ -612,7 +612,7 @@ export function HoverTypoBoldSelect(LetterSpacing) {
           </div>
        </div>
        <div  id="typo-bold-hover-effects-button"
-          class="sc-bg-3f3f3f sc-mt-3 sc-relative sc-items-center sc-flex sc-border-hover-3d3d3d sc-border sc-border-solid sc-border-3f3f3f sc-cursor-pointer sc-px-2 sc-justify-between sc-py-1 sc-rounded-4px">
+          class="sc-bg-3f3f3f sc-mt-2 sc-relative sc-items-center sc-flex sc-border-hover-3d3d3d sc-border sc-border-solid sc-border-3f3f3f sc-cursor-pointer sc-px-2 sc-justify-between sc-py-1 sc-rounded-4px">
           <p class="sc-roboto  sc-universal sc-font-size-14">Text Effect</p>
          <span  class="sc-arrow-placeholder sc-rotate-180 sc-w-10 sc-flex sc-items-center sc-justify-center"></span>
        </div>
