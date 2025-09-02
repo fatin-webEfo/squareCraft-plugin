15d19c8300b6f8de4d6c15752be256b0e147795f
Iam-Zarif <zarifraja19@gmail.com>
2025-09-02 13:56:48 +0600

    add

--- src/components/Typo/HoverTypoAllSelect.js
+++ src/components/Typo/HoverTypoAllSelect.js
@@ -51,58 +51,59 @@ export function HoverTypoAllSelect(fontSizes, LetterSpacing) {
          </div>
        </div>
      </div>
-     <div class="sc-items-center sc-gap-2 sc-mt-3 sc-flex">
-       <div class=" ">
-         <p class="sc-font-size-11 sc-font-thin sc-mt-4 sc-universal sc-text-gray-300 sc-roboto"> Background Color </p>
-         <div class="sc-col-span-5 sc-mt-2 sc-z-99999 sc-relative">
-           <div class="sc-flex sc-w-30 sc-justify-between sc-items-center sc-px-1 sc-bg-3f3f3f sc-inActiveTab-border sc-rounded-4px sc-py-4px">
-             <p class="sc-font-size-12 sc-roboto sc-universal">Select</p>
-             <div class="sc-square-6 sc-cursor-pointer"></div>
-           </div>
-           <div class="sc-absolute sc-z-99999 sc-border sc-hidden sc-border-solid sc-border-EF7C2F sc-top-12 sc-bg-3f3f3f sc-left-0 sc-p-1_5 sc-rounded-4px">
-             <div class="sc-button-fontcolor-arrow"></div>
-             <div class="sc-flex sc-items-center sc-justify-between">
-               <div class="sc-flex sc-relative sc-items-center sc-gap-1"></div>
-               <div class="sc-relative sc-inline-block sc-w-auto">
-                 <div class="sc-rounded-15px sc-px-2 sc-cursor-pointer sc-py-4px sc-bg-454545 sc-flex sc-items-center sc-gap-2">
-                   <p class="sc-universal sc-font-size-11 sc-roboto"> RGB </p>
-                   <span class="sc-arrow-placeholder sc-rotate-180"></span>
-                 </div>
-                 <div class="sc-absolute sc-border sc-border-solid sc-border-EF7C2F sc-left-0 sc-w-full sc-text-sm sc-z-999999 sc-rounded-15px sc-bg-494949 sc-flex sc-hidden sc-top-6 sc-flex-col sc-overflow-hidden">
-                   <div class="sc-cursor-pointer sc-dropdown-item sc-py-2px sc-px-2 sc-rounded-t-14px" data-format="RGB"> RGB </div>
-                   <div class="sc-cursor-pointer sc-dropdown-item sc-py-2px sc-px-2" data-format="HSL"> HSL </div>
-                   <div class="sc-cursor-pointer sc-dropdown-item sc-py-2px sc-px-2 sc-rounded-b-14px" data-format="HEX"> HEX </div>
-                 </div>
-               </div>
-             </div>
-             <div class="sc-h-1px sc-mt-2 sc-bg-color-gray"></div>
-             <div class="sc-flex color-h-selection sc-mt-2 sc-items-center sc-gap-2">
-               <div class="sc-relative">
-                 <div class="sc-w-2 sc-h-2 sc-absolute sc-cursor-pointer sc-rounded-full sc-border sc-border-solid sc-border-white"></div>
-               </div>
-               <div class="sc-h-full sc-w-3 sc-relative sc-rounded-15px">
-                 <div class="sc-absolute sc-w-5 sc-left-half sc-shadow-sm sc-rounded-15px sc-cursor-grabbing sc-h-2 sc-bg-color-f2f2f2"></div>
-               </div>
-               <div class="sc-h-full sc-w-3 sc-relative sc-rounded-15px">
-                 <div class="sc-absolute sc-w-5 sc-left-half sc-shadow-sm sc-rounded-15px sc-cursor-grabbing sc-h-2 sc-bg-color-f2f2f2"></div>
-               </div>
-             </div>
-             <div class="sc-flex sc-justify-between sc-mt-2 sc-px-2 sc-py-0_5 sc-rounded-4px sc-bg-454545">
-               <p class="sc-font-size-12 sc-roboto sc-font-light sc-universal"> Select </p>
-               <p class="sc-font-size-12 sc-roboto sc-font-light sc-universal"> 100% </p>
-             </div>
-           </div>
-         </div>
-       </div>
-       <div class=" ">
-         <p class="sc-font-size-11 sc-font-thin sc-mt-4 sc-universal sc-text-gray-300 sc-roboto"> Text Color </p>
-         <div class="sc-py-0_5 sc-mt-2 sc-w-30 sc-bg-3f3f3f sc-inActiveTab-border sc-flex sc-justify-between sc-items-center sc-px-1 sc-rounded-4px">
-           <p class="sc-font-size-12 sc-roboto c-font-light sc-universal"> #363544 </p>
+      <div class="sc-items-center sc-gap-2 sc-mt-3 sc-flex">
+     <div class=" ">
+       <p class="sc-font-size-11 sc-font-thin sc-mt-4 sc-universal sc-text-gray-300 sc-roboto"> Background Color </p>
+       <div class="sc-col-span-5 sc-mt-2 sc-z-99999 sc-relative">
+         <div class="sc-flex sc-w-30 sc-justify-between sc-items-center sc-px-1 sc-bg-3f3f3f sc-inActiveTab-border sc-rounded-4px sc-py-4px">
+           <p class="sc-font-size-12 sc-roboto sc-universal">Select</p>
            <div class="sc-square-6 sc-cursor-pointer"></div>
+         </div>
+         <div class="sc-absolute sc-z-99999 sc-border sc-hidden sc-border-solid sc-border-EF7C2F sc-top-12 sc-bg-3f3f3f sc-left-0 sc-p-1_5 sc-rounded-4px">
+           <div class="sc-button-fontcolor-arrow"></div>
+           <div class="sc-flex sc-items-center sc-justify-between">
+             <div class="sc-flex sc-relative sc-items-center sc-gap-1"></div>
+             <div class="sc-relative sc-inline-block sc-w-auto">
+               <div class="sc-rounded-15px sc-px-2 sc-cursor-pointer sc-py-4px sc-bg-454545 sc-flex sc-items-center sc-gap-2">
+                 <p class="sc-universal sc-font-size-11 sc-roboto"> RGB </p>
+                 <span class="sc-arrow-placeholder sc-rotate-180"></span>
+               </div>
+               <div class="sc-absolute sc-border sc-border-solid sc-border-EF7C2F sc-left-0 sc-w-full sc-text-sm sc-z-999999 sc-rounded-15px sc-bg-494949 sc-flex sc-hidden sc-top-6 sc-flex-col sc-overflow-hidden">
+                 <div class="sc-cursor-pointer sc-dropdown-item sc-py-2px sc-px-2 sc-rounded-t-14px" data-format="RGB"> RGB </div>
+                 <div class="sc-cursor-pointer sc-dropdown-item sc-py-2px sc-px-2" data-format="HSL"> HSL </div>
+                 <div class="sc-cursor-pointer sc-dropdown-item sc-py-2px sc-px-2 sc-rounded-b-14px" data-format="HEX"> HEX </div>
+               </div>
+             </div>
+           </div>
+           <div class="sc-h-1px sc-mt-2 sc-bg-color-gray"></div>
+           <div class="sc-flex color-h-selection sc-mt-2 sc-items-center sc-gap-2">
+             <div class="sc-relative">
+               <div class="sc-w-2 sc-h-2 sc-absolute sc-cursor-pointer sc-rounded-full sc-border sc-border-solid sc-border-white"></div>
+             </div>
+             <div class="sc-h-full sc-w-3 sc-relative sc-rounded-15px">
+               <div class="sc-absolute sc-w-5 sc-left-half sc-shadow-sm sc-rounded-15px sc-cursor-grabbing sc-h-2 sc-bg-color-f2f2f2"></div>
+             </div>
+             <div class="sc-h-full sc-w-3 sc-relative sc-rounded-15px">
+               <div class="sc-absolute sc-w-5 sc-left-half sc-shadow-sm sc-rounded-15px sc-cursor-grabbing sc-h-2 sc-bg-color-f2f2f2"></div>
+             </div>
+           </div>
+           <div class="sc-flex sc-justify-between sc-mt-2 sc-px-2 sc-py-0_5 sc-rounded-4px sc-bg-454545">
+             <p class="sc-font-size-12 sc-roboto sc-font-light sc-universal"> Select </p>
+             <p class="sc-font-size-12 sc-roboto sc-font-light sc-universal"> 100% </p>
+           </div>
          </div>
        </div>
      </div>
+     <div class=" ">
+       <p class="sc-font-size-11 sc-font-thin sc-mt-4 sc-universal sc-text-gray-300 sc-roboto"> Text Color </p>
+       <div class="sc-py-0_5 sc-mt-2 sc-w-30 sc-bg-3f3f3f sc-inActiveTab-border sc-flex sc-justify-between sc-items-center sc-px-1 sc-rounded-4px">
+         <p class="sc-font-size-12 sc-roboto c-font-light sc-universal"> #363544 </p>
+         <div class="sc-square-6 sc-cursor-pointer"></div>
+       </div>
+     </div>
    </div>
+   </div>
+  
    <div>
      <div id="typo-all-hover-border-button" class="sc-bg-3f3f3f sc-mt-2 sc-relative sc-z-9999 sc-flex sc-border-hover-3d3d3d sc-border sc-border-solid sc-border-3f3f3f sc-cursor-pointer sc-px-2 sc-justify-between sc-py-1 sc-rounded-4px">
        <p class="sc-roboto  sc-universal sc-font-size-14">Outline</p>
